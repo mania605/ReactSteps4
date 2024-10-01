d6a4692c53680995b6a3eeaf60d57573fba91899
tubi55 <tubi55@nate.com>
2024-10-01 11:16:35 +0900

    리액트 컴포넌트 안쪽의 JSX구문 실습

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,18 @@
 //5. 결국은 자바스크립트이기 때문에 JS예약어는 사용불가 (class)
 //6. 컴포넌트 호출은 먼저 import문으로 해당 함수명 가져온뒤 호출
 //7. 컴포넌트 호출 방식 <컴포넌트함수명 />
+//8. 만약 불필요한 wrappping요소 없이 그룹화하고 싶을때 (<></>) fragment로 감싸줌
 function App() {
 	return (
-		<div>
-			<h1>Hello</h1>
-		</div>
+		<>
+			<h1 className='abc'>Hello</h1>
+
+			<ul>
+				<li>list1</li>
+				<li>list2</li>
+				<li>list3</li>
+			</ul>
+		</>
 	);
 }
 
