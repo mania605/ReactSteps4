332e808540bdeabaae8e7c8716a05715acb2f028
tubi55 <tubi55@nate.com>
2024-10-01 16:46:46 +0900

    컴포넌트 라우트설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
 import Footer from './components/common/Footer';
 import Header from './components/common/Header';
 import Home from './components/main/Home';
-import Contact from './components/sub/Contact';
 import Gallery from './components/sub/Gallery';
 import Members from './components/sub/Members';
 import Posts from './components/sub/Posts';
 import Youtube from './components/sub/Youtube';
+import Contact from './components/sub/Contact';
+import { Route, Routes } from 'react-router-dom';
 
 export default function App() {
 	return (
 		<>
 			<Header />
 
-			<Home />
-			<Members />
-			<Gallery />
-			<Youtube />
-			<Contact />
-			<Posts />
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/members' element={<Members />} />
+				<Route path='/gallery' element={<Gallery />} />
+				<Route path='/youtube' element={<Youtube />} />
+				<Route path='/contact' element={<Contact />} />
+				<Route path='/posts' element={<Posts />} />
+			</Routes>
 
 			<Footer />
 		</>
