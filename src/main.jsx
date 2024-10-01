85c321a65151a77112f233c3e9e6c5168bd65ad1
tubi55 <tubi55@nate.com>
2024-10-01 10:48:43 +0900

    main.jsx를 통해 index.html로 리액트 요소가 넘어가는 흐름

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./styles/index.css";
+//가상돔요소를 생성해주는 리액트 객체 가져옴
+import React from 'react';
+//리액트객체가 생성한 리액트요소를 일반돔으로 변환하는 객체
+import ReactDOM from 'react-dom/client';
+//루트 컴포넌트 (리액트요소를 반환해주는 함수)
+import App from './App.jsx';
+//루트 컴포넌트에 적용할 전체 스타일파일
+import './styles/index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//App이라는 마스터컴포넌트를 돔형태로 구현해서 index.html의 #root요소안에 넣어줌
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
