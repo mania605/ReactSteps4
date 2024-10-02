0c571c98e0b26586b6895b367bb184ec433eaf13
tubi55 <tubi55@nate.com>
2024-10-02 14:17:16 +0900

    멤버화면 출력을 위한 데이터 따로 추출

---
+++ src/data/memberData.js
@@ -0,0 +1,12 @@
+export default [
+	{ name: 'David Choi', pic: 'david.jpg', position: 'CEO' },
+	{ name: 'Paul Yoon', pic: 'paul.jpg', position: 'CFO' },
+	{ name: 'Emily Chung', pic: 'emily.jpg', position: 'CTO' },
+	{ name: 'Peter Smith', pic: 'peter.jpg', position: 'UI Designer' },
+	{ name: 'Julia Min', pic: 'julia.jpg', position: 'Frontend Dev' },
+	{ name: 'Michael Hall', pic: 'michael.jpg', position: 'Backend Dev' },
+	{ name: 'Emma Smith', pic: 'emma.jpg', position: 'Project Manager' }
+];
+
+//JSX : 화면에 요소를 출력하기 위한 JSX를 반환해주는 컴포넌트 함수 파일
+//JS : 일반적인 데이터 파일
