425bc7e3ad9ac7bd03c7a98a397622861af2f655
tubi55 <tubi55@nate.com>
2024-10-02 11:11:14 +0900

    배열 데이터를 통한 반복 JSX처리

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,9 +1,8 @@
 import { FaEnvelope, FaInstagramSquare, FaYoutube } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-//React의 Link컴포넌트를 활용해서 메뉴이동처리 하면
-//불필요한 서버요청 없이 미리 한번에 불러온 컴포넌트를 실시간으로 클라이언트단에서 변경처리 가능
 
 export default function Header() {
+	const gnbArr = ['members', 'gallery', 'youtube', 'contact', 'posts'];
 	return (
 		<header className='header'>
 			<h1>
@@ -12,21 +11,13 @@ export default function Header() {
 
 			<nav>
 				<ul className='gnb'>
-					<li>
-						<Link to={'/members'}>MEMBERS</Link>
-					</li>
-					<li>
-						<Link to={'/gallery'}>GALLERY</Link>
-					</li>
-					<li>
-						<Link to={'/youtube'}>YOUTUBE</Link>
-					</li>
-					<li>
-						<Link to={'/contact'}>CONTACT</Link>
-					</li>
-					<li>
-						<Link to={'/posts'}>POSTS</Link>
-					</li>
+					{gnbArr.map((data, idx) => {
+						return (
+							<li key={idx}>
+								<Link to={'/' + data}>{data.toUpperCase()}</Link>
+							</li>
+						);
+					})}
 				</ul>
 
 				<ul className='sns'>
@@ -44,3 +35,21 @@ export default function Header() {
 		</header>
 	);
 }
+
+/*
+	<li>
+		<Link to={'/members'}>MEMBERS</Link>
+	</li>
+	<li>
+		<Link to={'/gallery'}>GALLERY</Link>
+	</li>
+	<li>
+		<Link to={'/youtube'}>YOUTUBE</Link>
+	</li>
+	<li>
+		<Link to={'/contact'}>CONTACT</Link>
+	</li>
+	<li>
+		<Link to={'/posts'}>POSTS</Link>
+	</li>
+*/
