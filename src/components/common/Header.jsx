c33c7682d140ecb0be4eae766f55fe8f9427328e
tubi55 <tubi55@nate.com>
2024-10-02 11:38:35 +0900

    메뉴 배열 데이터 반복처리

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
 	const gnbArr = ['members', 'gallery', 'youtube', 'contact', 'posts'];
+	const snsArr = [FaEnvelope, FaInstagramSquare, FaYoutube];
 	return (
 		<header className='header'>
 			<h1>
 				<Link to={'/'}>ALPACO</Link>
 			</h1>
-
 			<nav>
 				<ul className='gnb'>
 					{gnbArr.map((data, idx) => {
@@ -21,35 +21,16 @@ export default function Header() {
 				</ul>
 
 				<ul className='sns'>
-					<li>
-						<FaYoutube />
-					</li>
-					<li>
-						<FaInstagramSquare />
-					</li>
-					<li>
-						<FaEnvelope />
-					</li>
+					{snsArr.map((Data, idx) => {
+						return (
+							<li key={idx}>
+								{/* snsArr에서 반복을 돌면서 Data파라미로 전달되는 각각의 객체는 컴포넌트 함수 아래와같이 JSX문으로 호출 가능 이때 컴포넌트 규칙에 따라 파라미터명도 대문자로 시작해서 호출 */}
+								<Data />
+							</li>
+						);
+					})}
 				</ul>
 			</nav>
 		</header>
 	);
 }
-
-/*
-	<li>
-		<Link to={'/members'}>MEMBERS</Link>
-	</li>
-	<li>
-		<Link to={'/gallery'}>GALLERY</Link>
-	</li>
-	<li>
-		<Link to={'/youtube'}>YOUTUBE</Link>
-	</li>
-	<li>
-		<Link to={'/contact'}>CONTACT</Link>
-	</li>
-	<li>
-		<Link to={'/posts'}>POSTS</Link>
-	</li>
-*/
