8dbc0f1efb9dcb12dbc9e20916ab19229c0acdfa
tubi55 <tubi55@nate.com>
2024-10-02 15:50:12 +0900

    멤버 페이지 스타일링 중간 단계

--- src/components/sub/Members.jsx
+++ src/components/sub/Members.jsx
@@ -4,7 +4,6 @@ import memberData from '../../data/memberData';
 export default function Members() {
 	return (
 		<Layout title={'MEMBERS'}>
-			{/* 첫번째 데이터만 뽑아서 출력 */}
 			<article className='ceoBox'>
 				<div className='txt'>
 					<h2>{memberData[0].name}</h2>
@@ -15,10 +14,17 @@ export default function Members() {
 				</div>
 			</article>
 
-			<article className='memberListbox'>
+			<article className='memberListBox'>
+				<div className='titBox'>
+					<h2>Our Team Members</h2>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora possimus non ipsa cum. Veritatis, dolore
+						aliquam? Consectetur assumenda dolor labore.
+					</p>
+				</div>
+
 				<ul>
 					{memberData.map((member, idx) => {
-						//첫번째 순번의 데이터가 아닐때에만 반복출력
 						if (idx !== 0) {
 							return (
 								<li key={idx}>
@@ -34,7 +40,18 @@ export default function Members() {
 						}
 					})}
 				</ul>
+
+				<div className='descBox'>
+					<h2>Lorem ipsum dolor sit.</h2>
+					<p>
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. A esse cupiditate, vitae deleniti repellat
+						explicabo sit, corrupti beatae dicta, nulla optio corporis alias. Perferendis quidem sapiente minima,
+						quisquam inventore soluta.
+					</p>
+				</div>
 			</article>
 		</Layout>
 	);
 }
+// 미션 (3시 40분까지)
+// 카페에 있는 멤버페이지 하단 영역(memberListBox)을 최대한 비슷하게 스타일링
