bced1477905a28bff307060c06cfce5f383f21a1
tubi55 <tubi55@nate.com>
2024-10-02 14:20:55 +0900

    멤버 데이터 반복 출력

--- src/components/sub/Members.jsx
+++ src/components/sub/Members.jsx
@@ -1,9 +1,21 @@
 import Layout from '../common/Layout';
+import memberData from '../../data/memberData';
 
 export default function Members() {
+	console.log(memberData);
 	return (
 		<Layout title={'MEMBERS'}>
 			<p>Members Page contents come here.</p>
+
+			{/* <article>
+				<div className="pic">
+					<img src="이미지" alt="" />
+				</div>
+				<div className="txt">
+					<h2>사람 이름</h2>
+					<p>직책</p>
+				</div>
+			</article> */}
 		</Layout>
 	);
 }
