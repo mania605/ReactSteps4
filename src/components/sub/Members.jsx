6f77cb879a12850bb3570dc1c3810cd65673f9d2
tubi55 <tubi55@nate.com>
2024-10-02 14:41:31 +0900

    멤버 데이터 반복 출력

--- src/components/sub/Members.jsx
+++ src/components/sub/Members.jsx
@@ -5,17 +5,22 @@ export default function Members() {
 	console.log(memberData);
 	return (
 		<Layout title={'MEMBERS'}>
-			<p>Members Page contents come here.</p>
-
-			{/* <article>
-				<div className="pic">
-					<img src="이미지" alt="" />
-				</div>
-				<div className="txt">
-					<h2>사람 이름</h2>
-					<p>직책</p>
-				</div>
-			</article> */}
+			{memberData.map((member, idx) => {
+				return (
+					<article key={idx}>
+						<div className='pic'>
+							<img src={'/' + member.pic} alt={member.name} />
+						</div>
+						<div className='txt'>
+							<h2>{member.name}</h2>
+							<p>{member.position}</p>
+						</div>
+					</article>
+				);
+			})}
 		</Layout>
 	);
 }
+
+//미션 (2시 35분 까지)
+//-위의 구조로 반복 출력하도록 배열과 .map함수를 이용해서 구현
