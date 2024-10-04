052dfa509a3c744b8d101c150bd85d1b9c5b9351
tubi55 <tubi55@nate.com>
2024-10-04 16:14:53 +0900

    상세페이지에 데이터 호출시 초기 렌더링시 객체값 비어있는 오류 해결을 위한 옵셔널 체이닝처리

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -1,6 +1,6 @@
 export default function Layout({ title, children }) {
 	return (
-		<main className={title.toLowerCase()}>
+		<main className={title?.toLowerCase()}>
 			<h1>{title}</h1>
 
 			<section>{children}</section>
--- src/components/sub/YoutubeDetail.jsx
+++ src/components/sub/YoutubeDetail.jsx
@@ -1,28 +1,43 @@
 import { useParams } from 'react-router-dom';
 import Layout from '../common/Layout';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import useCombineText from '../../../hooks/useCombineText';
 
 export default function YoutubeDetail() {
-	//useParms로 url을 통해 전달되는 파라미터값을 반환
-	// 주소/youtube/abc (abc라는 값을 params객체로 전달받음)
+	//해당 컴포넌트 2번 재랜더링됨
+	//YoubutVid상태값이 null상태로 렌더링되고 그때 fecthing가 받아온 데이터를 해당 상태에 담아주면서 2차 렌더링 발생
 	const { id } = useParams();
+	const [YoutubeVid, setYoutubeVid] = useState(null);
+	const combineText = useCombineText();
+	console.log(YoutubeVid);
 
 	useEffect(() => {
-		//이전 목록화면에서 제목 클릭시 전달되는 id값을 params로 받아서
-		//새로운 요청 url을 만들고 useEffect로 컴포넌트 마운트시 한번만 서버쪽에 데이터 요청후 배열값 전달 받음
 		const api_key = import.meta.env.VITE_YOUTUBE_API;
 		const url = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&id=${id}`;
 
 		fetch(url)
 			.then(data => data.json())
 			.then(json => {
-				console.log(json);
+				setYoutubeVid(json.items[0]);
 			});
 	}, []);
 
+	//아래와 같이 첫번째 렌더링시에는 YoubutVid상태값이 null인 상태
+	//null요소의 snippet라는 프로퍼티 접근을 하는 것이기 때문에 문법 에러 발생
+	//두번째 렌더링시부터는 상태값이 담겨있으므로 에러발생하지 않음
+	//해결 방법 : 처음 렌더링시 초기 상태값이 비어있을때 오류해결 방법 (Optional chaining)
 	return (
-		<Layout title={'YOUTUBE DETAIL'}>
-			<p>Posts Page contents come here.</p>
+		<Layout title={YoutubeVid?.snippet.title}>
+			<figure>
+				<iframe
+					width='100%'
+					height='100%'
+					title='youtube'
+					src={`https://www.youtube.com/embed/${YoutubeVid?.snippet.resourceId.videoId}`}></iframe>
+			</figure>
+
+			<p>{YoutubeVid?.snippet.description}</p>
+			<span>{combineText(YoutubeVid?.snippet.publishedAt.split('T')[0], '-', '.')}</span>
 		</Layout>
 	);
 }
