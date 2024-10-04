3424ab82b6441cb0b9cda37c0ff078b7264e67cc
tubi55 <tubi55@nate.com>
2024-10-04 15:46:33 +0900

    다이나믹 라우터를 통한 유튜브 고유 id정보 상세페이지에 전달

--- src/components/sub/Youtube.jsx
+++ src/components/sub/Youtube.jsx
@@ -3,6 +3,7 @@ import Layout from '../common/Layout';
 import Pic from '../common/Pic';
 import useShortenText from '../../../hooks/useShortenText';
 import useCombineText from '../../../hooks/useCombineText';
+import { Link } from 'react-router-dom';
 
 export default function Youtube() {
 	const shortenText = useShortenText();
@@ -33,7 +34,9 @@ export default function Youtube() {
 			{Vids.map((vid, idx) => {
 				return (
 					<article key={idx}>
-						<h3>{shortenText(vid.snippet.title, 60)}</h3>
+						<h3>
+							<Link to={'/youtube/' + vid.id}>{shortenText(vid.snippet.title, 60)}</Link>
+						</h3>
 						<div className='txt'>
 							<p>{shortenText(vid.snippet.description, 150)}</p>
 							<span>{combineText(vid.snippet.publishedAt.split('T')[0], '-', '.')}</span>
--- src/components/sub/YoutubeDetail.jsx
+++ src/components/sub/YoutubeDetail.jsx
@@ -1,11 +1,24 @@
 import { useParams } from 'react-router-dom';
 import Layout from '../common/Layout';
+import { useEffect } from 'react';
 
 export default function YoutubeDetail() {
 	//useParms로 url을 통해 전달되는 파라미터값을 반환
 	// 주소/youtube/abc (abc라는 값을 params객체로 전달받음)
-	const params = useParams();
-	console.log(params);
+	const { id } = useParams();
+
+	useEffect(() => {
+		//이전 목록화면에서 제목 클릭시 전달되는 id값을 params로 받아서
+		//새로운 요청 url을 만들고 useEffect로 컴포넌트 마운트시 한번만 서버쪽에 데이터 요청후 배열값 전달 받음
+		const api_key = import.meta.env.VITE_YOUTUBE_API;
+		const url = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&id=${id}`;
+
+		fetch(url)
+			.then(data => data.json())
+			.then(json => {
+				console.log(json);
+			});
+	}, []);
 
 	return (
 		<Layout title={'YOUTUBE DETAIL'}>
@@ -13,3 +26,7 @@ export default function YoutubeDetail() {
 		</Layout>
 	);
 }
+
+//미션
+//위에서 반환받은 데이터로 상세 데이터 호출
+//제목, iframe 영상, 본문, 날짜 순으로 출력
