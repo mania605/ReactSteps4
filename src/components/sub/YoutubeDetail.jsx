c075dcd4fb5afb3cf8601689ec8a613844a6a58c
tubi55 <tubi55@nate.com>
2024-10-04 17:19:02 +0900

    useState, useEffect를 활용한 서버 데이터 fetching후 화면에 반복출력하는 패턴 문법(암기 필수)

--- src/components/sub/YoutubeDetail.jsx
+++ src/components/sub/YoutubeDetail.jsx
@@ -40,9 +40,9 @@ export default function YoutubeDetail() {
   1. useState를 이용해서 state에 값을 옮겨담고 state변경함수로 state값 변경처리
   2. useEffect구문의 구조를 파악하고 의존성 배열의 역할
   3. useState와 useEffect를 활용해서 서버 데이터 fetching처리후 state에 담기
-  4. 커스텀훅 만드는 방법 (커스텀훅의 조건 및 호출 위치)
-  5. useShortenText 커스텀훅 사용방법
-  6. useCombineText 커스텀훅 사용방법
-  7. 다이나믹 라우터를 이용해서  /youtube/:id를 활용해서 상세페이지에 특정 id값 전달하는 방법
-  8. 상태값에 있는 객체를 불러올떄 ?. 형태로 옵셔녈 체이닝을 처리하는 이유
+  4. 다이나믹 라우터를 이용해서  /youtube/:id를 활용해서 상세페이지에 특정 id값 전달하는 방법
+  5. 상태값에 있는 객체를 불러올떄 ?. 형태로 옵셔녈 체이닝을 처리하는 이유
+  6. 커스텀훅 만드는 방법  (선택사항)
+  7. useShortenText 커스텀훅 사용방법 (선택사항)
+  8. useCombineText 커스텀훅 사용방법 (선택사항)
 */
