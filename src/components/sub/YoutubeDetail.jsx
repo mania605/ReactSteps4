e173347e863c8f586c2d1d9f46a124a2f44c73bf
tubi55 <tubi55@nate.com>
2024-10-04 16:46:28 +0900

    자가진단 항목

--- src/components/sub/YoutubeDetail.jsx
+++ src/components/sub/YoutubeDetail.jsx
@@ -4,8 +4,6 @@ import { useEffect, useState } from 'react';
 import useCombineText from '../../../hooks/useCombineText';
 
 export default function YoutubeDetail() {
-	//해당 컴포넌트 2번 재랜더링됨
-	//YoubutVid상태값이 null상태로 렌더링되고 그때 fecthing가 받아온 데이터를 해당 상태에 담아주면서 2차 렌더링 발생
 	const { id } = useParams();
 	const [YoutubeVid, setYoutubeVid] = useState(null);
 	const combineText = useCombineText();
@@ -36,3 +34,15 @@ export default function YoutubeDetail() {
 		</Layout>
 	);
 }
+
+/*
+  자가진단 항목
+  1. useState를 이용해서 state에 값을 옮겨담고 state변경함수로 state값 변경처리
+  2. useEffect구문의 구조를 파악하고 의존성 배열의 역할
+  3. useState와 useEffect를 활용해서 서버 데이터 fetching처리후 state에 담기
+  4. 커스텀훅 만드는 방법 (커스텀훅의 조건 및 호출 위치)
+  5. useShortenText 커스텀훅 사용방법
+  6. useCombineText 커스텀훅 사용방법
+  7. 다이나믹 라우터를 이용해서  /youtube/:id를 활용해서 상세페이지에 특정 id값 전달하는 방법
+  8. 상태값에 있는 객체를 불러올떄 ?. 형태로 옵셔녈 체이닝을 처리하는 이유
+*/
