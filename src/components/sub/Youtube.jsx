ec7f31ba670e08a2488c956f9b8950326b5ea0c5
tubi55 <tubi55@nate.com>
2024-10-04 13:29:48 +0900

    유튜브 제목 말줄임 처리

--- src/components/sub/Youtube.jsx
+++ src/components/sub/Youtube.jsx
@@ -5,18 +5,22 @@ import Pic from '../common/Pic';
 export default function Youtube() {
 	const [Vids, setVids] = useState([]);
 
-	const api_key = import.meta.env.VITE_YOUTUBE_API;
-	const pid = 'PLHtvRFLN5v-W5bQjvyH8QTdQQhgflJ3nu';
-	const num = 10;
-	const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${pid}&key=${api_key}&maxResults=${num}`;
+	const fetchYoutube = () => {
+		const api_key = import.meta.env.VITE_YOUTUBE_API;
+		const pid = 'PLHtvRFLN5v-W5bQjvyH8QTdQQhgflJ3nu';
+		const num = 10;
+		const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${pid}&key=${api_key}&maxResults=${num}`;
 
-	useEffect(() => {
 		fetch(url)
 			.then(data => data.json())
 			.then(json => {
 				console.log(json.items);
 				setVids(json.items);
 			});
+	};
+
+	useEffect(() => {
+		fetchYoutube();
 	}, []);
 
 	return (
@@ -25,7 +29,7 @@ export default function Youtube() {
 				return (
 					<article key={idx}>
 						<Pic className='thumb' src={vid.snippet.thumbnails.high.url} />
-						<h3>{vid.snippet.title}</h3>
+						<h3>{vid.snippet.title.length >= 60 ? vid.snippet.title.substr(0, 60) + '...' : vid.snippet.title}</h3>
 						<p>{vid.snippet.description}</p>
 						<span>{vid.snippet.publishedAt}</span>
 					</article>
