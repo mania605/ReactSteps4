0f166655cbda55619a947b8c953bb1cef9ffdd42
tubi55 <tubi55@nate.com>
2024-10-04 10:40:57 +0900

    유튜브 데이터를 fetching후 state에 담는 구문을 useEffect없이 실행시, 무한 데이터 호출이 발생하는 원인 파악

--- src/components/sub/Youtube.jsx
+++ src/components/sub/Youtube.jsx
@@ -2,31 +2,24 @@ import { useState, useEffect } from 'react';
 import Layout from '../common/Layout';
 
 export default function Youtube() {
-	const [Num, setNum] = useState(0);
+	const [Vids, setVids] = useState([]);
+	console.log(Vids);
 
-	useEffect(() => {
-		//의존성배열이 비어있는 useEffect안쪽의 콜백함수는 컴포넌트 마운트시 한번만 호출됨
-		console.log('Mounted');
+	const api_key = 'AIzaSyCtJt2jnOcXV6eLUZmF2gT6LGa3mSPkpbM';
+	const pid = 'PLHtvRFLN5v-W5bQjvyH8QTdQQhgflJ3nu';
+	const num = 10;
+	const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${pid}&key=${api_key}&maxResults=${num}`;
 
-		return () => {
-			//clean-up함수 해당 컴포넌트 언마운트시 실행될 함수
-			console.log('UnMounted');
-		};
-	}, []);
+	// 아래와 같이 서버에서 가져오 데이터를 컴포넌트 안쪽에서 활용하기 위해 state담는 구문을 useEffect를 사용하지 않고 바로 활용하면
+	// 데이터를 무한 호출하는 이슈가 발생
+	// 미션 - 아래 코드를 호출해서 무한데이터 호출이 발생하는 현상을 확인한 뒤, 위와 같인 이슈가 발행하는 원인 고민 (45분까지)
 
-	useEffect(() => {
-		//의존성배열에 등록된 Num값이 변경될때마다 useEffect안쪽의 콜백함수 호출됨
-		console.log('Num값 변경될떄다마 호출');
-	}, [Num]);
+	// fetch(url)
+	// 	.then(data => data.json())
+	// 	.then(json => {
+	// 		const youtubeArr = json.items;
+	// 		setVids(youtubeArr);
+	// 	});
 
-	return (
-		<Layout title={'YOUTUBE'}>
-			<h2>{Num}</h2>
-			<button onClick={() => setNum(Num + 1)}>changeNum</button>
-		</Layout>
-	);
+	return <Layout title={'YOUTUBE'}></Layout>;
 }
-
-/*
-	useEffect (생명주기 관리함수 : 생성(Mount), 변경(ReRender), 소멸(UnMount))
-*/
