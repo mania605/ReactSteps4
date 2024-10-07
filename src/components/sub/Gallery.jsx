1e269bc8bf1e0b5017ea90050ba84604eac016a8
tubi55 <tubi55@nate.com>
2024-10-07 11:09:06 +0900

    API키값 환경변수에 등록하여 배포시 중요한 정보값 숨기기

--- src/components/sub/Gallery.jsx
+++ src/components/sub/Gallery.jsx
@@ -7,7 +7,7 @@ export default function Gallery() {
 
 	useEffect(() => {
 		const method = 'flickr.people.getPhotos';
-		const flickr_api = '21e294ad0ec03a32d7355980457d9e11';
+		const flickr_api = import.meta.env.VITE_FLICKR_API;
 		const myID = '197119297@N02';
 		const num = 10;
 		const url = `https://www.flickr.com/services/rest/?method=${method}&api_key=${flickr_api}&user_id=${myID}&per_page=${num}&nojsoncallback=1&format=json`;
