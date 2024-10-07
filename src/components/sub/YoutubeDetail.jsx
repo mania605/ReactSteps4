c918e37d3867a73afb1af25d5b4fd94e77d248a2
tubi55 <tubi55@nate.com>
2024-10-07 09:38:33 +0900

    useState, useEffect훅 조합으로 외부 데이터 fetching후 화면 렌더링하는 흐름 정리

--- src/components/sub/YoutubeDetail.jsx
+++ src/components/sub/YoutubeDetail.jsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from 'react';
 import useCombineText from '../../../hooks/useCombineText';
 
 export default function YoutubeDetail() {
-	console.log('detail');
 	const { id } = useParams();
 	const [YoutubeVid, setYoutubeVid] = useState(null);
 	const combineText = useCombineText();
@@ -36,16 +35,3 @@ export default function YoutubeDetail() {
 		</Layout>
 	);
 }
-
-/*
-	동적 라우터 (Dynamic Router) 
-	- 상세페이지 같은 컨텐츠를 출력해야 될때 각각의 상세페이지 컨텐츠에 대응되는 컴포넌트를 만드는 것은 비효율적
-	-  /:id 같은 형식으로 특정 URL 뒤에 값을 비어있는 상세페이지 컴포넌트에 전달
-	- 상세 페이지 컴포넌트에서는 id값을 useParams() 훅을 통해 전달 받음
-	- 이렇게 전달받은 고유 id 값을 이용해서 새롭게 상세 페이지 전용 데이터를 요청해서 받은 뒤 출력하는 형식
-
-	동적 라우터를 확인해야 되는 컴포넌트 순서
-	1. App.jsx에서 동적 라우터 패턴 확인 (:파라미터명)
-	2. Youtube.jsx에서 클릭한 요소의 Link에 적용되어있는 이동 URL 확인
-	3. YoutubeDetail.jsx에서 useParams()훅 호출한 뒤, 전달된 id값을 추출하고 해당 id값을 활용한 상세페이지 전용 컨텐츠 데이터 요청
-*/
