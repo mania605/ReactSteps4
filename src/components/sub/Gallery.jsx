4c0c80a276dde976a09ebc119a113292eb9a2f00
tubi55 <tubi55@nate.com>
2024-10-07 15:26:41 +0900

    상태값 변경에 따른 스크롤 기능 활성화, 비활성화 처리

--- src/components/sub/Gallery.jsx
+++ src/components/sub/Gallery.jsx
@@ -21,6 +21,15 @@ export default function Gallery() {
 			});
 	}, []);
 
+	//의존성 배열에 ModalOpen 상태값을 연결해서 모달창이 열리고 닫힐때마다
+	//body요소의 스크롤바 기능 여부를 분기처리
+	//정리 : 리액트는 HTML,JS작업방식처럼 직접적인 DOM을 제어하는 방식이 아닌 State의 변경에 따라 간접적으로 기능이 구현되는 패턴을 주로 사용
+	//위와 같이 state에 따라 UI의 기능 화면이 변경되는 로직의 패턴을 사용하면
+	//복잡한 대단위 프로젝트에서 state상태값만 관리하면 되기에 업무 채산성, 효율성이 높아짐
+	useEffect(() => {
+		document.body.style.overflow = ModalOpen ? 'hidden' : 'auto';
+	}, [ModalOpen]);
+
 	return (
 		<>
 			<Layout title={'GALLERY'}>
@@ -44,8 +53,3 @@ export default function Gallery() {
 		</>
 	);
 }
-/*
-	미션 (3시 20분까지)
-	- 모달창 생성시 document.body.style.overflow= 'hidden'으로 처리해서 스크롤기능 비활성화처리
-	- 모달창 제거시 document.body.style.overflow='auto'로 처리해서 스크롤기능 다시 활성화
-*/
