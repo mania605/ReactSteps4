308f82a3cc000a88ae13427d6d0a5130dd3b3332
tubi55 <tubi55@nate.com>
2024-10-07 14:39:05 +0900

    부모에세 자식 컴포넌트에 상태변경함수를 전달해서 자식컴포넌트가 부모컴포넌트의 상태값 제어가능하게 처리

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -1,8 +1,12 @@
-export default function Modal({ children }) {
+export default function Modal({ children, setModalOpen }) {
 	return (
 		<aside className='modal'>
 			<div className='con'>{children}</div>
-			<button className='btnClose'>CLOSE</button>
+
+			{/* 상태 변경함수를 전달받아서 이벤트 발생시 부모의 상태값을 변경해서 닫기 처리 */}
+			<button className='btnClose' onClick={() => setModalOpen(false)}>
+				CLOSE
+			</button>
 		</aside>
 	);
 }
--- src/components/sub/Gallery.jsx
+++ src/components/sub/Gallery.jsx
@@ -5,7 +5,6 @@ import Modal from '../common/Modal';
 
 export default function Gallery() {
 	const [Flickr, setFlickr] = useState([]);
-	//모달 컴포넌트 출력여부를 결정할 state생성
 	const [ModalOpen, setModalOpen] = useState(false);
 
 	useEffect(() => {
@@ -41,13 +40,8 @@ export default function Gallery() {
 				</section>
 			</Layout>
 
-			{/* ModalOpen 상태값이 true일때에만 Modal컴포넌트를 호출해서 출력 */}
-			{ModalOpen && <Modal>FLICKR IMAGE</Modal>}
+			{/* 자식 컴포넌트인 모달 안쪽에서 부모인 ModalOpen상태값을 변경해야 되기 때문에 상태변경함수 자체를 전달 */}
+			{ModalOpen && <Modal setModalOpen={setModalOpen}>FLICKR IMAGE</Modal>}
 		</>
 	);
 }
-
-//미션 (2시 30분까기 고민)
-//자식 컴포넌트인 Modal안쪽에서 닫기 버튼 클릭시 부모에 있는 ModalOpen이라는 상태값을 false로 변경해서 모달창 닫는 로직 고민
-//리액트에서 데이터는 부모에서 자식으로 데이터를 전달할 수 있는 단방향 데이터 방식임을 유의
-//부모에서 Modal이란 컴포넌트에 어떤형태의 정보를 전달해야지 자식에서 해당 정보를 바탕으로 자기 자신을 언마운트 처리할지를 고민
