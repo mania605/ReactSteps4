4d9963e684cfa784d3551835032f04a2fd8c5acd
tubi55 <tubi55@nate.com>
2024-10-08 11:28:19 +0900

    문자열 분리 커스텀훅 사용

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -1,19 +1,21 @@
 import { useLocation } from 'react-router-dom';
-import { motion } from 'framer-motion';
+import useSplitText from '../../hooks/useSplitText';
+import { useEffect, useRef } from 'react';
 
 export default function Layout({ title, children }) {
+	const ref_title = useRef(null);
+	//커스텀훅으로 핸들러함수 안쪽에서 호출할 수 있는 실제사용가능한 함수 반환받음
+	const splitText = useSplitText();
 	const { pathname } = useLocation();
 	const isDetail = pathname.includes('/youtube/');
 
+	useEffect(() => {
+		splitText(ref_title);
+	}, []);
+
 	return (
 		<main className={isDetail ? 'detail' : title.toLowerCase()}>
-			<motion.h1
-				initial={{ x: -200, opacity: 0 }}
-				animate={{ x: 0, opacity: 1 }}
-				exit={{ scale: 2, opacity: 0, transition: { duration: 0.3 } }}
-				transition={{ duration: 1, ease: 'easeIn' }}>
-				{title}
-			</motion.h1>
+			<h1 ref={ref_title}>{title}</h1>
 
 			<section>{children}</section>
 		</main>
