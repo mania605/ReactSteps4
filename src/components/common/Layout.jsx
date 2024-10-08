51290f96da4fca77c1f990835723a32791dd2ac8
tubi55 <tubi55@nate.com>
2024-10-08 12:16:05 +0900

    텍스트모션 오류 해결

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -3,34 +3,21 @@ import useSplitText from '../../hooks/useSplitText';
 import { useEffect, useRef } from 'react';
 
 export default function Layout({ title, children }) {
-	const ref_frame = useRef(null);
 	const ref_title = useRef(null);
 	const splitText = useSplitText();
 	const { pathname } = useLocation();
 	const isDetail = pathname.includes('/youtube/');
 
 	useEffect(() => {
+		//훅 자체적으로 참조객체 요소 활성화 처리
 		splitText(ref_title);
-
-		setTimeout(() => {
-			ref_frame.current.classList.add('on');
-		}, 0);
 	}, []);
 
 	return (
-		<main ref={ref_frame} className={isDetail ? 'detail' : title.toLowerCase()}>
+		<main className={isDetail ? 'detail' : title.toLowerCase()}>
 			<h1 ref={ref_title}>{title}</h1>
 
 			<section>{children}</section>
 		</main>
 	);
 }
-
-/*
-	motion 컴포넌트에서 자주 쓰는 스타일 속성
-	x: 가로축 이동 (숫자, 퍼센트는 문자열 처리)
-	y: 세로축 이동
-	scale:확대
-	rotate: 회전
-	opacity: 투명도
-*/
--- src/hooks/useSplitText.js
+++ src/hooks/useSplitText.js
@@ -8,5 +8,20 @@ export default function useSplitText() {
 		}
 
 		ref.current.innerHTML = tags;
+
+		//매번 호출하는 부모 컴포넌트에서 on을 추가하는 구문이 번거로우므로
+		//훅 자체적으로 해당 요소 자체에 on을 추가
+		setTimeout(() => {
+			ref.current.classList.add('on');
+		}, 100);
 	};
 }
+
+/*
+  setTimeout의 delay값을 0으로만 줘도 
+  해당 코드는 무조건 ref.current.innerHTML = tags; 실행된뒤 호출됨 (동기화됨)
+  그럼에도 불구하고 100이라는 지연시간을 준 이유는
+  동기화 시점이 innerHTML로 동적 요소를 넣는 호출 시점일 뿐
+  실제 동적으로 DOM이 완료된 이후를 보장하진 않기 때문에
+  물리적으로 실제 돔으로 변환될 약간의 시간을 확보하기 위함
+*/
