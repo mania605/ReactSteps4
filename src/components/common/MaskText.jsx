f0dbfe6c6c2ca37cf13d332cd180b53a09aca0b5
tubi55 <tubi55@nate.com>
2024-10-08 15:42:26 +0900

    MaskText 스타일 객체 연결(오류버전)

--- src/components/common/MaskText.jsx
+++ src/components/common/MaskText.jsx
@@ -1,19 +1,47 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function MaskText({ children }) {
-	console.log('mask');
-	const ref_frame = useRef(null);
+	const [Mounted, setMounted] = useState(false);
+
+	const frameStyle = {
+		fontSize: '1.2rem',
+		fontFamily: 'orbitron',
+		color: '#555',
+		display: 'inline-block',
+		position: 'relative',
+		overflow: 'hidden',
+		marginBottom: 80
+	};
+	const conStyle = {
+		opacity: 0,
+		transitionDuration: '0.1s',
+		transitionDelay: '0.3s'
+	};
+	const maskStyle = {
+		width: '100%',
+		heigiht: '100%',
+		position: 'absolute',
+		top: 0,
+		left: '-100%',
+		backgroundColor: '#555',
+		transitionTimingFunction: 'linear',
+		transitionDuration: '0.6s'
+	};
+
+	//con, mask의 활성화 스타일 객체
+	const conStyleActive = { ...conStyle, opacity: 1 };
+	const maskStyleActive = { ...maskStyle, left: '100%' };
 
 	useEffect(() => {
 		setTimeout(() => {
-			ref_frame.current.classList.add('on');
-		}, 100);
+			setMounted(true);
+		}, 1000);
 	}, []);
 
 	return (
-		<div className='slogan' ref={ref_frame}>
-			<span>{children}</span>
-			<div className='mask'></div>
+		<div className='slogan' style={frameStyle}>
+			<span style={Mounted ? conStyleActive : conStyle}>{children}</span>
+			<div style={Mounted ? maskStyleActive : maskStyle}></div>
 		</div>
 	);
 }
