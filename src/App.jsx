530bc3bf755498df1c7a9fa05b8200efbbfab572
tubi55 <tubi55@nate.com>
2024-10-08 09:30:09 +0900

    framer-motion 기본 사용법

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,29 @@ import Members from './components/sub/Members';
 import Posts from './components/sub/Posts';
 import Youtube from './components/sub/Youtube';
 import Contact from './components/sub/Contact';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import YoutubeDetail from './components/sub/YoutubeDetail';
+import { AnimatePresence } from 'framer-motion';
 
 export default function App() {
+	const location = useLocation();
 	return (
 		<>
 			<Header />
 
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/members' element={<Members />} />
-				<Route path='/gallery' element={<Gallery />} />
-				<Route path='/youtube' element={<Youtube />} />
-				<Route path='/youtube/:id' element={<YoutubeDetail />} />
-				<Route path='/contact' element={<Contact />} />
-				<Route path='/posts' element={<Posts />} />
-			</Routes>
+			{/* 라우터를 통한 컴포넌트 전환시 이전 컴포넌트에 모션이 동작되고 있으면 해당 모션이 끝날때가지 컴포넌트 언마운트 시점을 지연처리 */}
+			<AnimatePresence mode='wait'>
+				{/* 라우터 이동시마다의 각 컴포넌트 고유값을 전달하기 위해서 각 컴포넌트마다의 path경로를 key로지정 */}
+				<Routes location={location} key={location.pathname}>
+					<Route path='/' element={<Home />} />
+					<Route path='/members' element={<Members />} />
+					<Route path='/gallery' element={<Gallery />} />
+					<Route path='/youtube' element={<Youtube />} />
+					<Route path='/youtube/:id' element={<YoutubeDetail />} />
+					<Route path='/contact' element={<Contact />} />
+					<Route path='/posts' element={<Posts />} />
+				</Routes>
+			</AnimatePresence>
 
 			<Footer />
 		</>
--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -1,15 +1,19 @@
 import { useLocation } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 export default function Layout({ title, children }) {
 	const { pathname } = useLocation();
-
-	//현재 url상의 path가 /youtube/를 포함하고 있드면 isDetail은 true, 그렇지 않으면 false
 	const isDetail = pathname.includes('/youtube/');
 
 	return (
-		// 현재 출력되는 컴포넌트가 상세페이지일때만 detail클래스를 적용하고 그렇지 않을때는 title값을 클래스로 적용
 		<main className={isDetail ? 'detail' : title.toLowerCase()}>
-			<h1>{title}</h1>
+			<motion.h1
+				initial={{ x: -200, opacity: 0 }}
+				animate={{ x: 0, opacity: 1 }}
+				exit={{ scale: 2, opacity: 0, transition: { duration: 0.3 } }}
+				transition={{ duration: 1, ease: 'easeIn' }}>
+				{title}
+			</motion.h1>
 
 			<section>{children}</section>
 		</main>
