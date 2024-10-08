9f359093d79920d00a08f051c4fcddf7fa53937d
tubi55 <tubi55@nate.com>
2024-10-08 14:47:48 +0900

    마스크 리빌 모션 베이직

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -5,17 +5,27 @@ import { motion } from 'framer-motion';
 
 export default function Layout({ title, children }) {
 	const ref_title = useRef(null);
+	const ref_slogan = useRef(null);
 	const splitText = useSplitText();
 	const { pathname } = useLocation();
 	const isDetail = pathname.includes('/youtube/');
 
 	useEffect(() => {
 		splitText(ref_title, { interval: 0.1 });
+		ref_slogan.current.classList.add('on');
 	}, []);
 
 	return (
 		<main className={isDetail ? 'detail' : title.toLowerCase()}>
 			<h1 ref={ref_title}>{title}</h1>
+
+			<div className='slogan' ref={ref_slogan}>
+				<span>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
+					repellendus.
+				</span>
+				<div className='mask'></div>
+			</div>
 
 			<motion.section
 				initial={{ opacity: 0, y: 200 }}
@@ -27,3 +37,9 @@ export default function Layout({ title, children }) {
 		</main>
 	);
 }
+
+/*
+	slgoan프레임이 활성화되면 (on클래스 붙으면)
+	마스크박스가 왼쪽밖에서 오른쪽밖으로 1초동안 등속이속
+	마스크가 절반이동한 시점인 0.5초시점에 span텍스트를 보임처리
+*/
