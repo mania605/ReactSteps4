47ac22e10ffd89c1bc8d919af6b83591c520fa67
tubi55 <tubi55@nate.com>
2024-10-10 15:14:09 +0900

    Members페이지에 컨텐츠 영역만 Content컴포넌트로 wrapping처리해서 페이드 모션 적용

---
+++ src/components/common/Content.jsx
@@ -0,0 +1,17 @@
+import { motion } from 'framer-motion';
+
+export default function Content({ children, duration = 0.5, delay = 0 }) {
+	//motion data
+	const { init, active, end, time } = {
+		init: { opacity: 0, y: 200 },
+		active: { opacity: 1, y: 0 },
+		end: { opacity: 0, y: 200, transition: { delay: 0 } },
+		time: { duration: duration, delay: delay }
+	};
+
+	return (
+		<motion.div className='content' initial={init} animate={active} exit={end} transition={time}>
+			{children}
+		</motion.div>
+	);
+}
--- src/components/sub/Members.jsx
+++ src/components/sub/Members.jsx
@@ -3,6 +3,7 @@ import memberData from '../../data/memberData';
 import Pic from '../common/Pic';
 import MaskBox from '../common/MaskBox';
 import MaskText from '../common/MaskText';
+import Content from '../common/Content';
 
 export default function Members() {
 	return (
@@ -13,51 +14,53 @@ export default function Members() {
 				Lorem ipsum dolor
 			</MaskText>
 
-			<article className='ceoBox'>
-				<div className='txt'>
-					<h2>{memberData[0].name}</h2>
-					<p>{memberData[0].position}</p>
-				</div>
+			<Content delay={1.5}>
+				<article className='ceoBox'>
+					<div className='txt'>
+						<h2>{memberData[0].name}</h2>
+						<p>{memberData[0].position}</p>
+					</div>
 
-				<MaskBox style={{ width: '50%', height: '65vh' }} delay={2}>
-					<Pic style={{ width: '100%', height: '100%' }} src={'/' + memberData[0].pic} />
-				</MaskBox>
-			</article>
+					<MaskBox style={{ width: '50%', height: '65vh' }} delay={2 + 1.5}>
+						<Pic style={{ width: '100%', height: '100%' }} src={'/' + memberData[0].pic} />
+					</MaskBox>
+				</article>
 
-			<article className='memberListBox'>
-				<div className='titBox'>
-					<h2>Our Team Members</h2>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora possimus non ipsa cum. Veritatis, dolore aliquam? Consectetur
-						assumenda dolor labore.
-					</p>
-				</div>
+				<article className='memberListBox'>
+					<div className='titBox'>
+						<h2>Our Team Members</h2>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora possimus non ipsa cum. Veritatis, dolore aliquam? Consectetur
+							assumenda dolor labore.
+						</p>
+					</div>
 
-				<ul>
-					{memberData.map((member, idx) => {
-						if (idx !== 0) {
-							return (
-								<li key={idx}>
-									{/* 이미지 컴포넌트 호출후 src에 이미지 url값 전달, pic클래스에는 이미지의 크기정도만 지정 */}
-									<Pic src={member.pic} className='pic' shadow={true} />
-									<div className='txt'>
-										<h2>{member.name}</h2>
-										<p>{member.position}</p>
-									</div>
-								</li>
-							);
-						}
-					})}
-				</ul>
+					<ul>
+						{memberData.map((member, idx) => {
+							if (idx !== 0) {
+								return (
+									<li key={idx}>
+										{/* 이미지 컴포넌트 호출후 src에 이미지 url값 전달, pic클래스에는 이미지의 크기정도만 지정 */}
+										<Pic src={member.pic} className='pic' shadow={true} />
+										<div className='txt'>
+											<h2>{member.name}</h2>
+											<p>{member.position}</p>
+										</div>
+									</li>
+								);
+							}
+						})}
+					</ul>
 
-				<div className='descBox'>
-					<h2>Lorem ipsum dolor sit.</h2>
-					<p>
-						Lorem ipsum, dolor sit amet consectetur adipisicing elit. A esse cupiditate, vitae deleniti repellat explicabo sit, corrupti
-						beatae dicta, nulla optio corporis alias. Perferendis quidem sapiente minima, quisquam inventore soluta.
-					</p>
-				</div>
-			</article>
+					<div className='descBox'>
+						<h2>Lorem ipsum dolor sit.</h2>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. A esse cupiditate, vitae deleniti repellat explicabo sit, corrupti
+							beatae dicta, nulla optio corporis alias. Perferendis quidem sapiente minima, quisquam inventore soluta.
+						</p>
+					</div>
+				</article>
+			</Content>
 		</Layout>
 	);
 }
