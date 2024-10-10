625f8292988cfbd7df8998292208a17ff4eb399b
tubi55 <tubi55@nate.com>
2024-10-10 11:49:18 +0900

    초기 페이지 전환 모션에 따른 세무 모션 딜레이 시간 정리

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -12,7 +12,8 @@ export default function Layout({ title, children }) {
 	const isDetail = pathname.includes('/youtube/');
 
 	useEffect(() => {
-		splitText(ref_title, { interval: 0.1 });
+		// 순서2. 마스크 모션이 끝날때 바로 제목 타이핑 모션
+		splitText(ref_title, { interval: 0.1, delay: 0.5 });
 	}, [splitText]);
 
 	return (
@@ -20,26 +21,29 @@ export default function Layout({ title, children }) {
 			<main className={isDetail ? 'detail' : title.toLowerCase()}>
 				<h1 ref={ref_title}>{title}</h1>
 
-				<MaskText duration={0.5} delay={0} color={'#444'} style={{ fontSize: 20, fontFamily: 'arial' }}>
+				{/* 순서3. 텍스트 타이핑 모션 끝날 시점에 첫줄 텍스트 마스크 모션 시작 */}
+				<MaskText delay={1} color={'#444'} style={{ fontSize: 20, fontFamily: 'arial' }}>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, ad?
 				</MaskText>
 				<br />
 
-				<MaskText duration={0.5} delay={0.5} color={'#444'} style={{ marginBottom: 120 }}>
+				{/* 순서4. 첫줄 텍스트 마스크 모션 끝날때 둘째줄 텍스트 마스크 모션 시작 */}
+				<MaskText delay={1.5} color={'#444'} style={{ marginBottom: 120 }}>
 					Lorem ipsum dolor
 				</MaskText>
 
+				{/* 순서5. 두번째 줄 마스크 모션 끝날떄쯤 전체 컨텐츠 영상 위쪽으로 페이드인 시작 */}
 				<motion.section
 					initial={{ opacity: 0, y: 200 }}
 					animate={{ opacity: 1, y: 0 }}
 					exit={{ opacity: 0, y: 200, transition: { delay: 0 } }}
-					transition={{ duration: 1, delay: 0.7, ease: 'linear' }}>
+					transition={{ duration: 1, delay: 1.5, ease: 'linear' }}>
 					{children}
 				</motion.section>
 			</main>
 
-			{/* 다른 요소와는 다르게 전체 페이지를 덮을 때에는 Mask요소가 브라우저를 기준으로 위치가 배치되어야 하므로 fixed속성으로 변경 */}
-			<Mask style={{ position: 'fixed' }} />
+			{/* 순번1- 페이지 전환시 바로 전체화면을 가리는 마스크모션 실행 */}
+			<Mask duration={0.5} delay={0} style={{ position: 'fixed' }} />
 		</>
 	);
 }
