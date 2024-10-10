84c1e501a2af4b754317cd0f07c3f6b0c0adcf04
mania605 <kim.minsun83@gmail.com>
2024-10-10 17:34:51 +0900

    전송 이벤트 발생시 참조객체에 담아놓은 DOM의 value값을 비워주는 초기화함수 호출

--- src/components/common/MailForm.jsx
+++ src/components/common/MailForm.jsx
@@ -1,24 +1,42 @@
+import { useRef } from 'react';
+
 export default function MailForm() {
+	const ref_name = useRef(null);
+	const ref_email = useRef(null);
+	const ref_msg = useRef(null);
+
+	//전송 이벤트 발생시 폼요소의 값을 비우기 위한 초기화 함수
+	const resetForm = () => {
+		[ref_name, ref_email, ref_msg].forEach(dom => (dom.current.value = ''));
+	};
+
+	//전송 버튼 클릭시 실행될 함수
+	const sendForm = e => {
+		e.preventDefault();
+		resetForm();
+	};
+
 	return (
 		<article className='mailForm'>
 			<div className='formBox'>
-				<form>
+				{/* form에 전송 이벤트 연결 */}
+				<form onSubmit={sendForm}>
 					{/* 문의자이름, 메일주소 입력받는 상단 영역 */}
 					<div className='upper'>
 						<span>
 							<label htmlFor='uName'>Name</label>
-							<input type='text' id='uName' placeholder='Leave your name' />
+							<input ref={ref_name} type='text' id='uName' placeholder='Leave your name' />
 						</span>
 						<span>
 							<label htmlFor='uMail'>E-Mail</label>
-							<input type='text' id='uMail' placeholder='Leave your email' />
+							<input ref={ref_email} type='text' id='uMail' placeholder='Leave your email' />
 						</span>
 					</div>
 
 					{/* 문의내용 입력받는 textarea 하단 영역 */}
 					<div className='lower'>
 						<label htmlFor='msg'>Message</label>
-						<textarea name='msg' id='msg' placeholder='Leave your message'></textarea>
+						<textarea ref={ref_msg} name='msg' id='msg' placeholder='Leave your message'></textarea>
 					</div>
 
 					{/* 전송,취소 버튼 그룹 */}
