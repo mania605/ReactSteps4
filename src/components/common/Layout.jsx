f7576eb6e5b25b8e01cf9f610a9e781b8dc46cdd
tubi55 <tubi55@nate.com>
2024-10-10 14:44:32 +0900

    서브 페이지내 특정 영역만 모션을 공통으로 처리하고 싶을떄의 방안 주석 정리

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -1,6 +1,4 @@
 import { useLocation } from 'react-router-dom';
-import { motion } from 'framer-motion';
-import MaskText from './MaskText';
 import Mask from './Mask';
 import SplitText from './SplitText';
 
@@ -12,23 +10,7 @@ export default function Layout({ title, children }) {
 		<>
 			<main className={isDetail ? 'detail' : title.toLowerCase()}>
 				<SplitText delay={0.5}>{title}</SplitText>
-
-				<MaskText delay={1} color={'#444'} style={{ fontSize: 20, fontFamily: 'arial' }}>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, ad?
-				</MaskText>
-				<br />
-				<MaskText delay={1.5} color={'#444'} style={{ marginBottom: 120 }}>
-					Lorem ipsum dolor
-				</MaskText>
-
-				{/* 순서5. 두번째 줄 마스크 모션 끝날떄쯤 전체 컨텐츠 영상 위쪽으로 페이드인 */}
-				<motion.section
-					initial={{ opacity: 0, y: 200 }}
-					animate={{ opacity: 1, y: 0 }}
-					exit={{ opacity: 0, y: 200, transition: { delay: 0 } }}
-					transition={{ duration: 1, delay: 1.5, ease: 'linear' }}>
-					{children}
-				</motion.section>
+				<section>{children}</section>
 			</main>
 
 			<Mask duration={0.5} delay={0} style={{ position: 'fixed' }} />
--- src/components/sub/Members.jsx
+++ src/components/sub/Members.jsx
@@ -2,19 +2,24 @@ import Layout from '../common/Layout';
 import memberData from '../../data/memberData';
 import Pic from '../common/Pic';
 import MaskBox from '../common/MaskBox';
+import MaskText from '../common/MaskText';
 
 export default function Members() {
 	return (
 		<Layout title={'MEMBERS'}>
+			<MaskText delay={1}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, ad?</MaskText>
+			<br />
+			<MaskText delay={1.5} style={{ marginBottom: 80 }}>
+				Lorem ipsum dolor
+			</MaskText>
+
 			<article className='ceoBox'>
 				<div className='txt'>
 					<h2>{memberData[0].name}</h2>
 					<p>{memberData[0].position}</p>
 				</div>
 
-				{/* MaskBox 안쪽에 Pic요소가 들어갈 경우 shadow속성 적용 불가: Mask frame자체가 내부 요소를 overflow:hidden 처리하기 때문 */}
 				<MaskBox style={{ width: '50%', height: '65vh' }} delay={2}>
-					{/* 직접 style객체를 props로 전달하여 불필요한 scss구문 추가 없이 스타일 적용 */}
 					<Pic style={{ width: '100%', height: '100%' }} src={'/' + memberData[0].pic} />
 				</MaskBox>
 			</article>
@@ -56,3 +61,13 @@ export default function Members() {
 		</Layout>
 	);
 }
+/*
+	미션
+	- MaskText컴포넌트 아래쪽의 전체 서브페이지의 콘텐츠를 일괄적으로 페이드모션 처리
+	- 해당 컨텐츠 모션을 모든 서브페이지 컴포넌트에 일일이 framer-motion적용하는 것은 비효율적
+	- Layout컴포넌트에 추가하지 않으면서 각각의 서브페이지 컴포넌트의 특정 부분에 복잡하고 일괄적인 프레임기능 적용 필요
+	- 작업1 : 서브페이지 컴포넌트 안쪽에서 컨텐츠 영역만 Wrapping처리해줄 Content.jsx 를 생성
+	- 작업2 : Context.jsx의 children으로 각 페이지의 콘텐츠를 전달해주도록 처리
+	- 작업3 : 각 서브페이지 컴포넌트에 Content컴포넌트를 Wrapper형태로 감싸서 각 페이지 전용 컨텐츠 내용 전달
+	- 작업4 : Content.jsx안쪽에 페이드인 효과의 motion컴포넌트 처리
+*/
