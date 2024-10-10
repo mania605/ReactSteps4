a464375b75793df091ce1bb69b05930c15f13ada
mania605 <kim.minsun83@gmail.com>
2024-10-10 16:04:21 +0900

    내정보로 덮어쓰기

--- src/components/sub/Gallery.jsx
+++ src/components/sub/Gallery.jsx
@@ -19,7 +19,7 @@ export default function Gallery() {
 	useEffect(() => {
 		const method = 'flickr.people.getPhotos';
 		const flickr_api = import.meta.env.VITE_FLICKR_API;
-		const myID = '197119297@N02';
+		const myID = import.meta.env.VITE_FLICKR_ID;
 		const num = 10;
 		const url = `https://www.flickr.com/services/rest/?method=${method}&api_key=${flickr_api}&user_id=${myID}&per_page=${num}&nojsoncallback=1&format=json`;
 
