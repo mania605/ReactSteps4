526d0110fe06fdd7cf8ed84f0829127bdf49e6a6
tubi55 <tubi55@nate.com>
2024-10-10 09:49:00 +0900

    framer-motion 옵션값 variants를 활용하여 외부 객체형태로 분리

--- src/components/common/MaskText.jsx
+++ src/components/common/MaskText.jsx
@@ -1,9 +1,7 @@
 import { motion } from 'framer-motion';
 
 export default function MaskText({ children, duration, delay, color, style }) {
-	//기본 스타일 객체
-	//외부 스타일 파일로 스타일 지정하면 해당 컴포넌트를 범용적으로 사용하기 번거로움
-	//이러한 문제점을 개선하기 위해 대안책 (styledComponent, tailwindCSS, 스타일 객체를 직접 내부에 생성)
+	//component styles
 	const frameStyle = {
 		fontSize: '1.2rem',
 		fontFamily: 'orbitron',
@@ -21,25 +19,36 @@ export default function MaskText({ children, duration, delay, color, style }) {
 		backgroundColor: color
 	};
 
+	//motion options
+	const spanMotion = {
+		in: { opacity: 0 },
+		on: { opacity: 1 },
+		out: { opacity: 0, transition: { delay: 0 } },
+		time: { duration: 0.01, delay: duration / 2 + delay }
+	};
+	const maskMotion = {
+		in: { x: '-101%' },
+		on: { x: '101%' },
+		time: { duration, delay }
+	};
+
 	return (
-		// 텍스트를 감싸주는 Wrapper
-		// 해당 모션 컴포넌트의 스타일을 부모컴포넌트에 호출시 편하게 변경처리 하기 위해서 전달받는 style 객체로 기존 style객체 덮어씀
 		<div style={{ ...frameStyle, ...style }}>
-			{/* children으로 전달된 실제 텍스트를 span으로 전달된 요소 */}
 			<motion.span
-				initial={{ opacity: 0 }}
-				animate={{ opacity: 1 }}
-				exit={{ opacity: 0, transition: { delay: 0 } }}
-				transition={{ duration: 0.01, delay: duration / 2 + delay }}>
+				variants={spanMotion}
+				initial='in'
+				animate='on'
+				exit='out'
+				transition={spanMotion.time}>
 				{children}
 			</motion.span>
 
-			{/* wrapper 안쪽에 실제 텍스트를 가져줄 마스크요소 */}
 			<motion.div
 				style={maskStyle}
-				initial={{ x: '-101%' }}
-				animate={{ x: '101%' }}
-				transition={{ duration, delay }}></motion.div>
+				variants={maskMotion}
+				initial='in'
+				animate='on'
+				transition={maskMotion.time}></motion.div>
 		</div>
 	);
 }
