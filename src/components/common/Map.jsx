abf549c329eac2b70e0aac1661407e47fdda1c94
mania605 <kim.minsun83@gmail.com>
2024-10-11 09:52:37 +0900

    에러 빨간에러 지도, 마커 생성 주석 설명

--- src/components/common/Map.jsx
+++ src/components/common/Map.jsx
@@ -4,12 +4,15 @@ export default function Map() {
 	const { kakao } = window;
 	const ref_mapFrame = useRef(null);
 
-	const mapOption = {
-		center: new kakao.maps.LatLng(33.450701, 126.570667)
-	};
+	const inst_postion = new kakao.maps.LatLng(33.450701, 126.570667);
+
+	const inst_marker = new kakao.maps.Marker({
+		position: inst_postion
+	});
 
 	useEffect(() => {
-		new kakao.maps.Map(ref_mapFrame.current, mapOption);
+		const inst_map = new kakao.maps.Map(ref_mapFrame.current, mapOption);
+		inst_marker.setMap(inst_map);
 	}, []);
 
 	return (
