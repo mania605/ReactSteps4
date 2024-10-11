70c3d5cb2ad43f1bbc1f66e40c6fd1e0ce96d732
mania605 <kim.minsun83@gmail.com>
2024-10-11 17:42:14 +0900

    코드 순서 재정렬 (기존 코드에서 지점 정보 변경시 마커 출력안되는 문제 해결)

--- src/components/common/Map.jsx
+++ src/components/common/Map.jsx
@@ -2,17 +2,13 @@ import { useEffect, useRef, useState } from 'react';
 
 export default function Map() {
 	const { kakao } = window;
+
+	//화면 렌더링에 필요한 state값 초기화
 	const [Index, setIndex] = useState(0);
 	const [Traffic, setTraffic] = useState(false);
-	//로드뷰 토글을 위한 state생성 초기값은 false로 로드뷰 초기 숨김처리
 	const [Roadview, setRoadview] = useState(false);
 
-	const ref_mapFrame = useRef(null);
-	const ref_viewFrame = useRef(null);
-	const ref_instMap = useRef(null);
-	//로드뷰 인스턴스가 담길 참조객체 생성
-	const ref_instView = useRef(null);
-	const ref_instClient = useRef(new kakao.maps.RoadviewClient());
+	//지점정보가 담긴 참조객체를 생성하고 현재 활성화된 Index순번의 데이터 추출
 	const ref_info = useRef([
 		{
 			title: 'COEX',
@@ -38,38 +34,47 @@ export default function Map() {
 	]);
 	const { latlng, markerImg, markerSize, markerPos } = ref_info.current[Index];
 
-	const inst_marker = new kakao.maps.Marker({
-		position: latlng,
-		image: new kakao.maps.MarkerImage(markerImg, markerSize, markerPos)
-	});
+	//순수함수 형태로 값을 바로 전달받을 수 있는 인스턴스값 참조객체에 담음
+	const ref_instClient = useRef(new kakao.maps.RoadviewClient());
+	const ref_instType = useRef(new kakao.maps.MapTypeControl());
+	const ref_instZoom = useRef(new kakao.maps.ZoomControl());
 
-	const instType = new kakao.maps.MapTypeControl();
-	const instZoom = new kakao.maps.ZoomControl();
+	//컴포넌트 마운트시에만 전달받을 수 있는 빈 참조객체 생성
+	const ref_mapFrame = useRef(null);
+	const ref_viewFrame = useRef(null);
+	const ref_instMap = useRef(null);
+	const ref_instMarker = useRef(null);
+	const ref_instView = useRef(null);
 
-	const initPos = () => {
-		console.log('initPos called!!');
-		ref_instMap.current.setCenter(latlng);
-	};
+	//리사이즈 이벤트에 연결될 화면위치 초기화 함수
+	const initPos = () => ref_instMap.current.setCenter(latlng);
 
+	//Index값이 변경될때마다 지도초기화, 뷰,마커, 로드뷰인스턴스 생성 및 리사이즈 이벤트연결
 	useEffect(() => {
-		// setTraffic(false);
-		// setRoadview(false);
-		//지점버튼 클릭해서 Index를 기반으로한 위치 인스턴스가 변경될때마다 Traffic, Roadvie상태 초기화해서 일반 지도화면 보이도록 처리
-		[setTraffic, setRoadview].forEach(func => func(false));
 		ref_mapFrame.current.innerHTML = '';
-		ref_instMap.current = new kakao.maps.Map(ref_mapFrame.current, { center: latlng });
-		inst_marker.setMap(ref_instMap.current);
-		[instType, instZoom].forEach(inst => ref_instMap.current.addControl(inst));
 
-		//roadview 인스턴스 생성
+		//맵, 마커, 로드뷰, 로드뷰 인스턴스 생성후 미리 생성한 참조객체에 옮겨담음
+		ref_instMap.current = new kakao.maps.Map(ref_mapFrame.current, { center: latlng });
+		ref_instMarker.current = new kakao.maps.Marker({
+			position: latlng,
+			image: new kakao.maps.MarkerImage(markerImg, markerSize, markerPos)
+		});
 		ref_instView.current = new kakao.maps.Roadview(ref_viewFrame.current);
-		//clientInstance의 getNearestPanoId함수 호출해서 현재 위치 인스턴스값 기준으로
-		//제일 가까운 panoId값을 찾아서 view인스턴스에 바인딩해서 로드뷰 화면에 출력
+
+		//마커 인스턴스에 지도 인스턴스 바인딩해서 지도에 마커 출력
+		ref_instMarker.current.setMap(ref_instMap.current);
+		//교통정보, 로드뷰 토글 상태 변경함수 반복호출해서 초기화 및 컨트롤러 반복호출하여 초기화
+		[setTraffic, setRoadview].forEach(func => func(false));
+		[ref_instType.current, ref_instZoom.current].forEach(inst => ref_instMap.current.addControl(inst));
+		//로드뷰 인스턴스에 panoId연결해 실제 로드뷰화면 출력하는 호출문
 		ref_instClient.current.getNearestPanoId(latlng, 50, panoId => ref_instView.current.setPanoId(panoId, latlng));
+
+		//윈도우 전역 객체에 resize 이벤트 핸들러 연결 및 제거
 		window.addEventListener('resize', initPos);
 		return () => window.removeEventListener('resize', initPos);
 	}, [Index]);
 
+	//Traffic 값이 반전될때마다 트레픽 레이어 토글
 	useEffect(() => {
 		Traffic
 			? ref_instMap.current.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
@@ -80,12 +85,13 @@ export default function Map() {
 		<section className='map'>
 			<h2>Location</h2>
 
+			{/* 맵, 로드뷰 프레임 */}
 			<figure className='mapFrame'>
-				{/* Roadview 상태값에 따라 지도화면 로드뷰화면 보임, 암보임 처리 */}
 				<article ref={ref_mapFrame} className={`mapFrame ${!Roadview && 'on'}`}></article>
 				<article ref={ref_viewFrame} className={`viewFrame ${Roadview && 'on'}`}></article>
 			</figure>
 
+			{/* 컨트롤 버튼 모음 */}
 			<nav className='btnSet'>
 				<ul className='branch'>
 					{ref_info.current.map((el, idx) => (
@@ -99,7 +105,6 @@ export default function Map() {
 					<li onClick={() => setTraffic(!Traffic)} className={Traffic ? 'on' : ''}>
 						{`Traffic ${Traffic ? 'OFF' : 'ON'}`}
 					</li>
-					{/* Roadview 상태값에 따라 버튼 활성화, 비활성화 처리 */}
 					<li onClick={() => setRoadview(!Roadview)} className={Roadview ? 'on' : ''}>
 						{`Roadview ${Roadview ? 'OFF' : 'ON'}`}
 					</li>
@@ -108,3 +113,4 @@ export default function Map() {
 		</section>
 	);
 }
+//미션 - 5시 40분까지 위의 분류된 카테고리 항목별로 코드 기능 분석 및 정리
