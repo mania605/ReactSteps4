b0a8c8488691a28b23921eb0e3a17d9c2bf22c17
mania605 <kim.minsun83@gmail.com>
2024-10-11 14:12:03 +0900

    코드원복2

--- src/components/common/Map.jsx
+++ src/components/common/Map.jsx
@@ -9,7 +9,7 @@ export default function Map() {
 	const ref_instMap = useRef(null);
 
 	//지도 정보 배열 참조객체 등록 및 비구조화할당으로 개별 정보 추출
-	const { current: ref_info } = useRef([
+	const ref_info = useRef([
 		{
 			title: 'COEX',
 			latlng: new kakao.maps.LatLng(37.5094091584729, 127.0624304750884),
@@ -32,7 +32,7 @@ export default function Map() {
 			markerPos: { offset: new kakao.maps.Point(116, 99) }
 		}
 	]);
-	const { latlng, markerImg, markerSize, markerPos } = ref_info[Index];
+	const { latlng, markerImg, markerSize, markerPos } = ref_info.current[Index];
 
 	//마커 인스턴스 생성
 	const inst_marker = new kakao.maps.Marker({
@@ -63,7 +63,7 @@ export default function Map() {
 
 			<nav className='btnSet'>
 				<ul className='branch'>
-					{ref_info.map((el, idx) => (
+					{ref_info.current.map((el, idx) => (
 						<li key={idx} className={idx === Index ? 'on' : ''} onClick={() => setIndex(idx)}>
 							{el.title}
 						</li>
