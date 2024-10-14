7897ca9d79c7cad2cb4caa5c67e23c90cdc8f294
mania605 <kim.minsun83@gmail.com>
2024-10-14 16:23:02 +0900

    한줄만 바꿔서?

--- src/hooks/useYoutube.js
+++ src/hooks/useYoutube.js
@@ -1,38 +1,38 @@
 import { useQuery } from '@tanstack/react-query';
 
-//아래 커스텀훅에서 활용될 fetching함수
-const fetchYoutube = async ({queryKey}) => {
-console.log(queryKey[1]);
-  const api_key = import.meta.env.VITE_YOUTUBE_API;
+//아래 커스텀훅에서 활용될 fetching 함수
+const fetchYoutube = async ({ queryKey }) => {
+	//useQuery로 전달된 opt객체는 fetching함수 안쪽의 queryKey로 전달받음
+	console.log(queryKey[1]); //{type:'A'}
+	const api_key = import.meta.env.VITE_YOUTUBE_API;
 	const baseURL = 'https://www.googleapis.com/youtube/v3/playlistItems';
-  const pidA = 'PLHtvRFLN5v-W5bQjvyH8QTdQQhgflJ3nu';
-  const pidB = 'PLHtvRFLN5v-W5bQjvyH8QTdQQhgflJ3nu';
+	const pidA = 'PLHtvRFLN5v-W5bQjvyH8QTdQQhgflJ3nu';
+	const pidB = 'PLYOPkdUKSFgUJeKpDIbI0H1wwgQO-hCZF';
 	const num = 10;
-  let url = '';
+	let url = '';
+	const urlA = `${baseURL}?part=snippet&playlistId=${pidA}&key=${api_key}&maxResults=${num}`;
+	const urlB = `${baseURL}?part=snippet&playlistId=${pidB}&key=${api_key}&maxResults=${num}`;
+	//전달된 queryKey 옵션값에 따라서 최종 요청 url변형후 fetch요청
+	queryKey[1].type === 'A' && (url = urlA);
+	queryKey[1].type === 'B' && (url = urlB);
 
-	const urlA = `${baseURL}?part=snippet&playlistId=${pidA}&key=${api_key}&maxResults=${num}`; 
-  const urlB = `${baseURL}?part=snippet&playlistId=${pidB}&key=${api_key}&maxResults=${num}`; 
-
-queryKey[1].type === 'A' && (url = urlA);
-queryKey[1].type === 'B' && (url = urlB);
-
-  const data=await fetch(url);
+	const data = await fetch(url);
 	const json = await data.json();
-	return json.items; 
-}; 
+	return json.items;
+};
 
-//useQuery기능이 내장된 실제 호출된 커스텀훅
-export const useYoutubeQuery = (opt={type:'A'}) =>{
+//useQuery기능이 내장된 실제 호출될 커스텀훅
+export const useYoutubeQuery = (opt = { type: 'B' }) => {
+	return useQuery({
+		//결과적으로 커스텀훅 호출시 옵션값을 쿼리키 배열의 두번쨰요소에 등록하면, 해당 옵션값에 따른 고유쿼리키가 자동 생성
+		queryKey: ['youtubeList', opt],
+		queryFn: fetchYoutube,
+		staleTime: 1000 * 60,
+		gcTime: 1000 * 60
+	});
+};
 
-  return useQuery({
-    queryKey: ['youtubeList', opt],
-    queryFn: fetchYoutube,
-    staleTime: 1000*60,  //1초 x 60 1분
-    gcTime: 1000*60
-  });
-}
-
-
+ 
 /*
 1. 유튜브 데이터 fetching 함수(전달될 인수값 필요) 
 
@@ -58,3 +58,5 @@ export const useYoutubeQuery = (opt={type:'A'}) =>{
 
 */
  
+
+//메인페이지인 해당 컴포넌트 안쪽에 useYoutubeQuery훅을 이용하여 유튜브 썸네일 미리보기 데이터를 3개만 출력
