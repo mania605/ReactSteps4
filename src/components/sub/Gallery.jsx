b4fef57e4b8dabc684cb5361171c1eb4265f8d6b
mania605 <kim.minsun83@gmail.com>
2024-10-14 17:12:59 +0900

    gallery컴포넌트에 opt추가로 마이갤러리, 인기갤러리 선별 호출

--- src/components/sub/Gallery.jsx
+++ src/components/sub/Gallery.jsx
@@ -9,26 +9,34 @@ export default function Gallery() {
 	const [ModalOpen, setModalOpen] = useState(false);
 	const [Index, setIndex] = useState(0);
 
-	//Gallery페이지에만 전용으로 동작할 커스텀 모션 객체 생성
-	//이때 end에 직접 추가해야 했던 delay구문은 제거하고 변경할 스타일 정보값만 전달
 	const customMotion = {
 		init: { opacity: 0, x: 200 },
 		active: { opacity: 1, x: 0 },
 		end: { opacity: 0, x: -200 }
 	};
 
-	useEffect(() => {
-		const method = 'flickr.people.getPhotos';
+	const fetchFlickr= async () =>{
+		const baseURL = 'https://www.flickr.com/services/rest/'
+		const method_mine= 'flickr.people.getPhotos';
+		const method_interest = 'flickr.interstingness.getList';
+
 		const flickr_api = import.meta.env.VITE_FLICKR_API;
 		const myID = import.meta.env.VITE_FLICKR_ID;
 		const num = 10;
-		const url = `https://www.flickr.com/services/rest/?method=${method}&api_key=${flickr_api}&user_id=${myID}&per_page=${num}&nojsoncallback=1&format=json`;
+		const urlMine= `${baseURL}?method=${method_mine}&api_key=${flickr_api}&user_id=${myID}&per_page=${num}&nojsoncallback=1&format=json`;
+		const urlInterest = `${baseURL}?method=${method_interest}&api_key=${flickr_api}&per_page=${num}&nojsoncallback=1&format=json`;
 
-		fetch(url)
-			.then(data => data.json())
-			.then(json => {
-				setFlickr(json.photos.photo);
-			});
+		opt.type === 'mine' &&(url = urlMine);
+		opt.type === 'interest' && (url = urlInterest);
+
+
+		const data = await fetch(url) ;
+		const json = await data.json();
+		setFlickr(json.photos.photo);
+	};
+
+	useEffect(() => {
+fetchFlickr();
 	}, []);
 
 	useEffect(() => {
@@ -37,8 +45,7 @@ export default function Gallery() {
 
 	return (
 		<>
-			<Layout title={'GALLERY'}>
-				{/* Content호출시 위에서 준비한 전용 모션 정보 props로 전달 */}
+			<Layout title={'GALLERY'}> 
 				<Content delay={1.5} customMotion={customMotion}>
 					<section className='galleryList'>
 						{Flickr.map((data, idx) => {
