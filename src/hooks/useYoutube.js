4197d7cfdd4d1ec77f0ef88d1c7766af0c2ee0c9
mania605 <kim.minsun83@gmail.com>
2024-10-14 14:45:23 +0900

    useQuery를 활용한 실제적인 유튜브데이터 불러오는 커스텀훅 완성

--- src/hooks/useYoutube.js
+++ src/hooks/useYoutube.js
@@ -1,22 +1,26 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery } from '@tanstack/react-query';
 
+//아래 커스텀훅에서 활용될 fetching함수
 const fetchYoutube = async () => {
-  const api_key = import.meta.env.VITE_YOUTUBE_API;
-  const pid = 'PLHtvRFLN5v-W5bQjvyH8QTdQQhgflJ3nu';
-  const num = 10;
-  const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${pid}&key=${api_key}&maxResults=${num}`;
-  
-  //promise를 반환하는 함수 앞쪽에 await등록하면 해당 함수가 아무리 시간이 오래걸려도 결과값이 완료될 때까지 다음 구문이 동기적으로 holding 됨
-  const data = await fetch(url) //프라미스를 반환하기 때문에 then을 쓸 수 있는 거다  앞에 await문을 붙이면 
-const json = await data.json();
-return json.items;
+	const api_key = import.meta.env.VITE_YOUTUBE_API;
+	const pid = 'PLHtvRFLN5v-W5bQjvyH8QTdQQhgflJ3nu';
+	const num = 10;
+	const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${pid}&key=${api_key}&maxResults=${num}`; 
 
-  //fetch(url)
-  //.then(data => data.json())
-  //.then(json => { 
-  //  return json.items;
-  //});
+	const json = await data.json();
+	return json.items; 
+}; 
+
+//useQuery기능이 내장된 실제 호출된 커스텀훅
+export const useYoutubeQuery = () =>{
+  return useQuery({
+    queryKey: ['youtubeList'],
+    queryFn: fetchYoutube,
+    staleTime: 1000*60,  //1초 x 60 1분
+    gcTime: 1000*60
+  });
 }
+
 
 /*
 유튜브 데이터 fetching 함수(전달될 인수값 필요) 
@@ -30,9 +34,4 @@ useQuery활용한 커스텀 훅 함수
 위의 useQuery를 활용한 커스텀훅으로 부터 반환된 데이터는 미리 설정한 옵션대로 브라우저단에서 캐싱처리되며 재활용됨
 데이터가 변경되면(요청url인 queryKey값이 달라지면) 다시 새롭게 fetching처리
 */
-
-
-
-export default function useYoutube(){
-  return(  );
-}
+ 
