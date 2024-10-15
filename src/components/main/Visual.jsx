c5a94c960f2c61de95c88491f64dfa040ed51f92
mania605 <kim.minsun83@gmail.com>
2024-10-15 15:40:11 +0900

    coverflow 설명 추가

--- src/components/main/Visual.jsx
+++ src/components/main/Visual.jsx
@@ -1,31 +1,32 @@
-
 import { useFlickrQuery } from '../../hooks/useFlickr';
 import Pic from '../common/Pic';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import 'swiper/css/effect-coverflow';
 import { EffectCoverflow } from 'swiper/modules';
 
 export default function Visual() {
 	const { data } = useFlickrQuery({ type: 'mine' });
 	return (
 		<figure className='visual'>
-			<Swiper          effect={'coverflow'}  spaceBetween={100}       slidesPerView={3} loop = {true}
-			
-			coverflowEffect={{
-				rotate: 50,
-				stretch: 0,
-				depth: 100,
-				modifier: 1,
-				slideShadows: true,
-			}}
-			modules={[EffectCoverflow]}
-			> {/*  spaceBetween은 사이 간격   slidesPerView는 한화면에 몇개씩 나오게 할 지 */}
-
+			<Swiper
+				slidesPerView={3}
+				spaceBetween={100}
+				loop={true}
+				effect={'coverflow'}
+				coverflowEffect={{
+					rotate: 50, //패널회전각도
+					stretch: 0, //패널간 당겨짐정도
+					depth: 100, //원근감정도
+					modifier: 1, //위 3가지 중첩강도 비율
+					slideShadows: true // 패널의 그림자
+				}}
+				modules={[EffectCoverflow]}>
 				{data?.map((pic, idx) => {
-					if (idx >= 10) return null;
+					if (idx >= 20) return null;
 					return (
 						<SwiperSlide key={idx}>
-							<Pic src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`} style={{ width: '100%', height: '100%' }} shadow/>
+							<Pic src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`} style={{ width: '100%', height: '100%' }} shadow />
 						</SwiperSlide>
 					);
 				})}
