2640654bae704f724897dfd3eeaf00311bcb740c
mania605 <kim.minsun83@gmail.com>
2024-10-15 15:41:34 +0900

    swiper기본 스타일로 원복

--- src/components/main/Visual.jsx
+++ src/components/main/Visual.jsx
@@ -1,9 +1,7 @@
 import { useFlickrQuery } from '../../hooks/useFlickr';
 import Pic from '../common/Pic';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import { EffectCoverflow } from 'swiper/modules';
+import 'swiper/css'; 
 
 export default function Visual() {
 	const { data } = useFlickrQuery({ type: 'mine' });
@@ -13,15 +11,7 @@ export default function Visual() {
 				slidesPerView={3}
 				spaceBetween={100}
 				loop={true}
-				effect={'coverflow'}
-				coverflowEffect={{
-					rotate: 50, //패널회전각도
-					stretch: 0, //패널간 당겨짐정도
-					depth: 100, //원근감정도
-					modifier: 1, //위 3가지 중첩강도 비율
-					slideShadows: true // 패널의 그림자
-				}}
-				modules={[EffectCoverflow]}>
+>
 				{data?.map((pic, idx) => {
 					if (idx >= 20) return null;
 					return (
