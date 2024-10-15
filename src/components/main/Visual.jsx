0ad211fcb1e3493887a850289128ddc36389a619
mania605 <kim.minsun83@gmail.com>
2024-10-15 15:36:29 +0900

    coverflow 이팩트 적용

--- src/components/main/Visual.jsx
+++ src/components/main/Visual.jsx
@@ -3,12 +3,23 @@ import { useFlickrQuery } from '../../hooks/useFlickr';
 import Pic from '../common/Pic';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import { EffectCoverflow } from 'swiper/modules';
 
 export default function Visual() {
 	const { data } = useFlickrQuery({ type: 'mine' });
 	return (
 		<figure className='visual'>
-			<Swiper      spaceBetween={100}       slidesPerView={4} loop = {true}> {/*  spaceBetween은 사이 간격   slidesPerView는 한화면에 몇개씩 나오게 할 지 */}
+			<Swiper          effect={'coverflow'}  spaceBetween={100}       slidesPerView={3} loop = {true}
+			
+			coverflowEffect={{
+				rotate: 50,
+				stretch: 0,
+				depth: 100,
+				modifier: 1,
+				slideShadows: true,
+			}}
+			modules={[EffectCoverflow]}
+			> {/*  spaceBetween은 사이 간격   slidesPerView는 한화면에 몇개씩 나오게 할 지 */}
 
 				{data?.map((pic, idx) => {
 					if (idx >= 10) return null;
