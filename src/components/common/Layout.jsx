89134f322efa45b05f63722a3e0e99ec08dbd043
mania605 <kim.minsun83@gmail.com>
2024-10-15 09:21:46 +0900

    메인 페이지에서만 전용 클래스명 적용

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -6,6 +6,15 @@ import { useEffect } from 'react';
 export default function Layout({ title, children }) {
 	const { pathname } = useLocation();
 	const isDetail = pathname.includes('/youtube/');
+	console.log(pathname);
+
+	let currentClass = '';
+
+	// path명을 통해 레이아웃에 다른 클래스명 적용
+if	(isDetail) currentClass = 'detail' ;
+else if (pathname=== '/') currentClass='main';
+else currentClass =title.toLowerCase();
+
 
 	//라우터를 통해 새로운 페이지 컴포넌트 마운트시 강제로 윈도우의 스크롤 위치값을 0으로 초기화
 	useEffect(() => {
@@ -14,7 +23,7 @@ export default function Layout({ title, children }) {
 	
 	return (
 		<>
-			<main className={isDetail ? 'detail' : title.toLowerCase()}>
+			<main className={currentClass}>
 				<SplitText delay={0.5}>{title}</SplitText>
 				<section>{children}</section>
 			</main>
