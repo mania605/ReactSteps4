ed27bef91f3fd7febab1399fd44181a63fb26acc
mania605 <kim.minsun83@gmail.com>
2024-10-15 15:25:46 +0900

    slidesPerView는 한화면에 몇개씩 나오게 할 지

--- src/components/main/Visual.jsx
+++ src/components/main/Visual.jsx
@@ -8,12 +8,13 @@ export default function Visual() {
 	const { data } = useFlickrQuery({ type: 'mine' });
 	return (
 		<figure className='visual'>
-			<Swiper>
+			<Swiper      spaceBetween={50}       slidesPerView={4}> {/*  spaceBetween은 사이 간격   slidesPerView는 한화면에 몇개씩 나오게 할 지 */}
+
 				{data?.map((pic, idx) => {
 					if (idx >= 10) return null;
 					return (
 						<SwiperSlide key={idx}>
-							<Pic src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`} style={{ width: '100%', height: '100%' }} />
+							<Pic src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`} style={{ width: '100%', height: '100%' }} shadow/>
 						</SwiperSlide>
 					);
 				})}
