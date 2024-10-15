4cfcc0878a3783c8af97eb1ff860c214eb749f8c
mania605 <kim.minsun83@gmail.com>
2024-10-15 13:52:25 +0900

    useFlickrQuery 동작 순서 주석 정리

--- src/components/sub/Gallery.jsx
+++ src/components/sub/Gallery.jsx
@@ -3,9 +3,13 @@ import Layout from '../common/Layout';
 import Pic from '../common/Pic';
 import Modal from '../common/Modal';
 import Content from '../common/Content';
-// import {useFlickrQuery} from '../../hooks/useFlickr';
+import { useFlickrQuery } from '../../hooks/useFlickr';
 
 export default function Gallery() {
+	//순서1- 갤러리 컴포넌트에 커스텀훅 호출시 전달해야되는 옵션 객체를 인수로 전달
+	const { data } = useFlickrQuery({ type: 'mine' });
+	console.log(data); //리액트쿼리훅이 데이터를 잘 반환하는지 확인
+
 	const ref_gallery = useRef(null);
 	const [Flickr, setFlickr] = useState([]);
 	const [ModalOpen, setModalOpen] = useState(false);
@@ -18,8 +22,7 @@ export default function Gallery() {
 		end: { opacity: 0, x: -200 }
 	};
 
-
-	const fetchFlickr = async opt => { 	// {type:'search', tag:'바다'}라는 객체 상태값이 opt로 전달됨
+	const fetchFlickr = async opt => {
 		const baseURL = 'https://www.flickr.com/services/rest/';
 		const method_mine = 'flickr.people.getPhotos';
 		const method_interest = 'flickr.interestingness.getList';
@@ -31,12 +34,10 @@ export default function Gallery() {
 		let url = '';
 		const urlMine = `${baseURL}?method=${method_mine}&api_key=${flickr_api}&user_id=${myID}&per_page=${num}&nojsoncallback=1&format=json`;
 		const urlInterest = `${baseURL}?method=${method_interest}&api_key=${flickr_api}&per_page=${num}&nojsoncallback=1&format=json`;
-		//검색전용 url값 추가
 		const urlSearch = `${baseURL}?method=${method_search}&api_key=${flickr_api}&per_page=${num}&nojsoncallback=1&format=json&tags=${opt.tag}`;
 
 		opt.type === 'mine' && (url = urlMine);
 		opt.type === 'interest' && (url = urlInterest);
-		//순서5: 전달된 opt값의 type이 search이므로 위에서 준비한 검색 전용 호출 url을 아래쪽 fetch함수에 전달에서 검색 데이터 요청
 		opt.type === 'search' && (url = urlSearch);
 
 		const data = await fetch(url);
@@ -44,27 +45,16 @@ export default function Gallery() {
 		setFlickr(json.photos.photo);
 	};
 
-
-
-	const handleSearch = e => { //기본전송기능을 막으면서 {type:'search', tag:'바다'}라는 객체값으로 Type상태값 변경처리
-		e.preventDefault(); //폼에서 전송 이벤트 발생시 이벤트가 발생한 form요소(e.target)의 첫번째 자식 요소인 input요소의 value값을 구해서
+	const handleSearch = e => {
+		e.preventDefault();
+		//폼에서 전송 이벤트 발생시 이벤트발생한 form(e.target)의 첫번재 자식 요소인 input요소의 value값을 구해서
+		//tag라는 프로퍼티에 담아서 Type 상태값 변경
+		//해당 값은 자동적으로 fetch함수안쪽의 검색요청 url의 쿼리값으로 등록됨
 		console.dir(e.target[0].value);
-if ('!e.target[0].value') return; // 검색어가 없으면 함수 종료
-		setType({ type: 'search', tag: e.target[0].value });//tag라는 프로퍼티에 담아서 Type상태값 변경, 해당 값은 자동적으로 fetch함수 안쪽의 검색요청url의 쿼리값으로 등록됨
-e.target[0].value = '';  // 검색어 제출 후 input 값을 비웁니다.
+		setType({ type: 'search', tag: e.target[0].value });
 	};
 
-	// const handleSearch = e => { 
-	// 	e.preventDefault(); 
-	// 	const searchTerm = e.target[0].value;
-	// 	if (!searchTerm) return; // 검색어가 없으면 함수 종료
-  // 	setType({ type: 'search', tag: e.target[0].value });//tag라는 프로퍼티에 담아서 Type상태값 변경, 해당 값은 자동적으로 fetch함수 안쪽의 검색요청url의 쿼리값으로 등록됨
-	// 	e.target[0].value = '';  // 검색어 제출 후 input 값을 비웁니다.
-	// };
-
-
-
-	useEffect(() => { //Type상태값 변경되면서 Type값은 내부의 fetchFlickr함수의 인수로 전달됨
+	useEffect(() => {
 		fetchFlickr(Type);
 		ref_gallery.current.classList.remove('on');
 
@@ -90,8 +80,8 @@ e.target[0].value = '';  // 검색어 제출 후 input 값을 비웁니다.
 								Interest Gallery
 							</li>
 						</ul>
-						
-						<form onSubmit={handleSearch}> {/* form안쪽의 button을 클릭하고 input에서 엔터치면 자동으로 wrapping요소인 form에 submit이벤트 발생됨 handleSearch호출 */}
+
+						<form onSubmit={handleSearch}>
 							<input type='text' placeholder='검색어를 입력하세요.' />
 							<button>search</button>
 						</form>
--- src/hooks/useFlickr.js
+++ src/hooks/useFlickr.js
@@ -1,89 +1,43 @@
+import { useQuery } from '@tanstack/react-query';
 
-
-import {useQuery} from '@tanstack/react-query';
+//미션 (1시 40분까지)
+//Gallery컴포넌트에 있는 fetch함수를 아래 fetchFlickr에 적용
+//Gallery컴포넌트에서 아래 훅 호출한 뒤 데이터 출력
 
 //fetchFn
-const fetchFlickr =({queryKey}) => {
-  //console.log(opt);//먼저 opt값 잘 전달되는지 확인
-  console.log({queryKey[1]}, // {type:'mine});
-    //opt {queryKey} 
-		const baseURL = 'https://www.flickr.com/services/rest/';
-		const method_mine = 'flickr.people.getPhotos';
-		const method_interest = 'flickr.interestingness.getList';
-		const method_search = 'flickr.photos.search';
+//순서4 - useQuery를 통해서 전달된 opt값을 비구조화할등으로 queryKey로 뽑아서 fetching함수 내부로 전달
+const fetchFlickr = async ({ queryKey }) => {
+	console.log(queryKey[1]); //{type:'mine'}
+	const baseURL = 'https://www.flickr.com/services/rest/';
+	const method_mine = 'flickr.people.getPhotos';
+	const method_interest = 'flickr.interestingness.getList';
+	const method_search = 'flickr.photos.search';
 
-		const flickr_api = import.meta.env.VITE_FLICKR_API;
-		const myID = '197119297@N02';
-		const num = 20;
-		let url = '';
-		const urlMine = `${baseURL}?method=${method_mine}&api_key=${flickr_api}&user_id=${myID}&per_page=${num}&nojsoncallback=1&format=json`;
-		const urlInterest = `${baseURL}?method=${method_interest}&api_key=${flickr_api}&per_page=${num}&nojsoncallback=1&format=json`;
-		//검색전용 url값 추가
-		const urlSearch = `${baseURL}?method=${method_search}&api_key=${flickr_api}&per_page=${num}&nojsoncallback=1&format=json&tags=${queryKey[1].tag}`;
+	const flickr_api = import.meta.env.VITE_FLICKR_API;
+	const myID = '197119297@N02';
+	const num = 20;
+	let url = '';
+	const urlMine = `${baseURL}?method=${method_mine}&api_key=${flickr_api}&user_id=${myID}&per_page=${num}&nojsoncallback=1&format=json`;
+	const urlInterest = `${baseURL}?method=${method_interest}&api_key=${flickr_api}&per_page=${num}&nojsoncallback=1&format=json`;
+	const urlSearch = `${baseURL}?method=${method_search}&api_key=${flickr_api}&per_page=${num}&nojsoncallback=1&format=json&tags=${queryKey[1].tag}`;
 
-		opt.type === 'mine' && (url = urlMine);
-		opt.type === 'interest' && (url = urlInterest);
-		//순서5: 전달된 opt값의 type이 search이므로 위에서 준비한 검색 전용 호출 url을 아래쪽 fetch함수에 전달에서 검색 데이터 요청
-		opt.type === 'search' && (url = urlSearch);
+	queryKey[1].type === 'mine' && (url = urlMine);
+	queryKey[1].type === 'interest' && (url = urlInterest);
+	queryKey[1].type === 'search' && (url = urlSearch);
 
-		const data = await fetch(url);
-		const json = await data.json();
-		setFlickr(json.photos.photo);
-	};
-
- 
-
-
-//useQuery를 활용한 실체 호출할 커스텀훅 등록
-export const function useFlickrQuery= opt =>{
-  return useQuery({
-    queryKey : ['flickrQuery',opt],
-    queryFn: fetchFlickr,
-    staleTime: 1000* 60,
-    gcTime: 1000 * 60   // 캐시타임이라고 하는 회사도 있음
-  });
+	const data = await fetch(url);
+	const json = await data.json();
+	//순서5- 해당 커스텀훅은 컴포넌트 외부에서 동작하는 함수이기 때문에 state함수 사용불가하고 단지 반환값을 내보내줌
+	//해당 값이 자동으로 useQuery쿼리에 의해서 캐싱처리된 상태로 컴포넌트로 전달됨
+	return json.photos.photo;
 };
 
-
-
-
-
-
-// import{useQuery} from '@tanstack/react-query';
-
-// const fetchFlickr = async opt => {
-//   //opt {queryKey} 
-// 		const baseURL = 'https://www.flickr.com/services/rest/';
-// 		const method_mine = 'flickr.people.getPhotos';
-// 		const method_interest = 'flickr.interestingness.getList';
-// 		const method_search = 'flickr.photos.search';
-
-// 		const flickr_api = import.meta.env.VITE_FLICKR_API;
-// 		const myID = '197119297@N02';
-// 		const num = 20;
-// 		let url = '';
-// 		const urlMine = `${baseURL}?method=${method_mine}&api_key=${flickr_api}&user_id=${myID}&per_page=${num}&nojsoncallback=1&format=json`;
-// 		const urlInterest = `${baseURL}?method=${method_interest}&api_key=${flickr_api}&per_page=${num}&nojsoncallback=1&format=json`;
-// 		//검색전용 url값 추가
-// 		const urlSearch = `${baseURL}?method=${method_search}&api_key=${flickr_api}&per_page=${num}&nojsoncallback=1&format=json&tags=${opt.tag}`;
-
-// 		opt.type === 'mine' && (url = urlMine);
-// 		opt.type === 'interest' && (url = urlInterest);
-// 		//순서5: 전달된 opt값의 type이 search이므로 위에서 준비한 검색 전용 호출 url을 아래쪽 fetch함수에 전달에서 검색 데이터 요청
-// 		opt.type === 'search' && (url = urlSearch);
-
-// 		const data = await fetch(url);
-// 		const json = await data.json();
-// 		setFlickr(json.photos.photo);
-// 	};
-
-//   export const useFlickrQuery = opt => {
-//     return useQuery({
-//     queryKey : ['flickrQuery', opt],
-//       queryFn: fetchFlickr,
-//       tatleTime: 1000 * 60,
-//       gcTime:1000 *60
-//     });
-//   };
-
-
+//순서2- 갤러리 컴포넌트로부터 전달받은 opt값을 내부 고유 쿼리키 생성하는데 활용
+export const useFlickrQuery = opt => {
+	return useQuery({
+		queryKey: ['flickrQuery', opt],
+		queryFn: fetchFlickr, //순서3 - 쿼리키로 전달된 opt값을 fetchFlickr함수에 전달하면서 호출
+		staleTime: 1000 * 60,
+		gcTime: 1000 * 60
+	});
+};
