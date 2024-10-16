ca2f7b529c2e36eb3a590f7e466b8cb918f77639
mania605 <kim.minsun83@gmail.com>
2024-10-16 13:43:53 +0900

    글씨크기가?

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,5 +1,6 @@
 import { FaBars, FaEnvelope, FaInstagram, FaYoutube } from 'react-icons/fa';
 import { Link, useLocation } from 'react-router-dom';
+import MobileMenu from './MobileMenu';
 
 export default function Header() {
 	const gnbArr = ['members', 'gallery', 'youtube', 'contact', 'posts'];
@@ -8,33 +9,36 @@ export default function Header() {
 	const { pathname } = useLocation();
 
 	return (
-		//메인전용 헤더에 변경되는 부분이 극히 적으므로 기존 header클래스를 베이스로 하고 메인페이지에서는 main클래스만 추가
-		<header className={"header ${pathname === '/' && 'main'}"}>
-			<h1>
-				<Link to={'/'}>ALPACO</Link>
-			</h1>
-			<nav>
-				<ul className='gnb'>
-					{gnbArr.map((data, idx) => {
-						return (
-							<li key={idx} className={pathname === '/' + data ? 'on' : ''}>
-								<Link to={'/' + data}>{data.toUpperCase()}</Link>
+		<>
+			<header className={`header ${pathname === '/' && 'main'}`}>
+				<h1>
+					<Link to={'/'}>ALPACO</Link>
+				</h1>
+				<nav>
+					<ul className='gnb'>
+						{gnbArr.map((data, idx) => {
+							return (
+								<li key={idx} className={pathname === '/' + data ? 'on' : ''}>
+									<Link to={'/' + data}>{data.toUpperCase()}</Link>
+								</li>
+							);
+						})}
+					</ul>
+
+					<ul className='sns'>
+						{snsArr.map((Data, idx) => (
+							<li key={idx}>
+								<Data />
 							</li>
-						);
-					})}
-				</ul>
+						))}
+					</ul>
+				</nav>
 
-				<ul className='sns'>
-					{snsArr.map((Data, idx) => (
-						<li key={idx}>
-							<Data />
-						</li>
-					))}
-				</ul>
-			</nav>
+				<FaBars className='btnMenuToggle' />
+			</header>
 
-			<FaBars className='btnMenuToggle' />
-		</header>
+			<MobileMenu />
+		</>
 	);
 }
 
