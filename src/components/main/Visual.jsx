866f2aea0f27403f6987f94b25081910159250f3
mania605 <kim.minsun83@gmail.com>
2024-10-16 11:17:04 +0900

    breakpoints를 이용해서 타블릿 구간에서 반응형 처리

--- src/components/main/Visual.jsx
+++ src/components/main/Visual.jsx
@@ -2,26 +2,15 @@ import { useFlickrQuery } from '../../hooks/useFlickr';
 import Pic from '../common/Pic';
 import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
 //Autoplay 모듈 가져옴
-import { Autoplay } from 'swiper/modules';
+import { Autoplay, Pagination } from 'swiper/modules';
 import { useState } from 'react';
 import 'swiper/css';
 import { FaPlay } from 'react-icons/fa';
-import { Pagination, Navigation } from 'swiper/modules';
 
-
-
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-
-
-//Swiper 컴포넌트 안쪽에서 호출할 자동롤링 시작 버튼 컴포넌트
 function BtnStart() {
-	//스와이퍼 전용 autoplay관련 메서드를 호출하기 위해서 useSwiper커스텀 훅으로 swiper 인스턴스 생성
 	const swiper = useSwiper();
-	console.log(swiper);
 
 	return (
-		//hidden(true: 숨김, false:보임), disabled(true:기능비활성화, false:기능활성화)
 		<button hidden={swiper.autoplay.running} className='btnStart' onClick={() => swiper.autoplay.start()}>
 			<FaPlay />
 		</button>
@@ -44,14 +33,21 @@ export default function Visual() {
 			</div>
 
 			<Swiper
-				modules={[Autoplay, Pagination, Navigation]}
-				pagination={{
-          type: 'fraction',
-        }}
-        navigation={false}
-				slidesPerView={3}
-				spaceBetween={100}
+				modules={[Autoplay, Pagination]}
+				pagination={{ type: 'fraction' }}
+				slidesPerView={1}
+				spaceBetween={0}
 				loop={true}
+				breakpoints={{
+					1000: {
+						slidesPerView: 2,
+						spaceBetween: 50
+					},
+					1400: {
+						slidesPerView: 3,
+						spaceBetween: 50
+					}
+				}}
 				centeredSlides={true}
 				onSlideChange={el => setIndex(el.realIndex)}
 				autoplay={{ delay: 2000, disableOnInteraction: true }}
@@ -72,8 +68,6 @@ export default function Visual() {
 				{/* 자동롤링 시작 버튼 컴포넌트 호출 */}
 				<BtnStart />
 			</Swiper>
-
-			
 		</figure>
 	);
 }
