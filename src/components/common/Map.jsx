bbd2d41af5fa2a49936a9e5f5a71e6122d4f2386
mania605 <kim.minsun83@gmail.com>
2024-10-17 09:21:36 +0900

    전역 state관리의 필요성

--- src/components/common/Map.jsx
+++ src/components/common/Map.jsx
@@ -41,6 +41,19 @@ export default function Map() {
 	const ref_instMarker = useRef(null);
 	const ref_instView = useRef(null);
 
+	//지도, 마커, 뷰 인스턴스 생성하는 메서드
+	//useCallback으로 함수 메모이제이션 출력할 인스턴스 정보가 변경될때에만 메모이제이션 풀림
+	//Index를 의존성 배열에 등록할 필요 없는 이유 (latlng, markerImg) 등등의 정보가 이미 Index상태값 변경될때마다 달라지는 값이고 해당 값을 의존성에 등록했기 때문
+	const createMap = useCallback(() => {
+		ref_mapFrame.current.innerHTML = '';
+		ref_instMap.current = new kakao.maps.Map(ref_mapFrame.current, { center: latlng });
+		ref_instMarker.current = new kakao.maps.Marker({ position: latlng, image: new kakao.maps.MarkerImage(markerImg, markerSize, markerPos) });
+		ref_instView.current = new kakao.maps.Roadview(ref_viewFrame.current);
+		ref_instMarker.current.setMap(ref_instMap.current);
+		[ref_instType.current, ref_instZoom.current].forEach(inst => ref_instMap.current.addControl(inst));
+		ref_instClient.current.getNearestPanoId(latlng, 50, panoId => ref_instView.current.setPanoId(panoId, latlng));
+	}, [kakao, latlng, markerImg, markerSize, markerPos]);
+
 	const initPos = useCallback(() => {
 		console.log('initPos');
 		ref_instMap.current.setCenter(latlng);
@@ -48,25 +61,15 @@ export default function Map() {
 
 	//useThrottle커스텀훅을 통해서 throttle이 적용된 새로운 throttledInitPos라는 함수 반환받음
 	const throttledInitPos = useThrottle(initPos);
-	const createMap = useCallback(() => {
-		ref_mapFrame.current.innerHTML = '';
-		ref_instMap.current = new kakao.maps.Map(ref_mapFrame.current, { center: latlng });
-		ref_instMarker.current = new kakao.maps.Marker({ position: latlng, image: new kakao.maps.MarkerImage(markerImg, markerSize, markerPos) });
-		ref_instView.current = new kakao.maps.Roadview(ref_viewFrame.current);
-		ref_instMarker.current.setMap(ref_instMap.current);
 
-		[setTraffic, setRoadview].forEach(func => func(false));
-		[ref_instType.current, ref_instZoom.current].forEach(inst => ref_instMap.current.addControl(inst));
-		ref_instClient.current.getNearestPanoId(latlng, 50, panoId => ref_instView.current.setPanoId(panoId, latlng));
-	}, [kakao, latlng, markerImg, markerSize, markerPos]);
-
+	//해당 useEffect에 Index의존성 배열 불필요한 이유
+	//이유 : 의존성배열에 createMap이 등록되어 있고 이미 createMap자체적으로 의존성배열에 Index에 따라 달라지는 값들을 등록되어 있음
+	//따라서 지도를 그리는데 필요한 Index상태값 기반의 정보값이 바뀌면 새롭게 바뀐 내용으로 createMap이 호출되고 그렇지 않으면 메모이제이션 함수 자체 재호출
 	useEffect(() => {
 		createMap();
-
-		//throttle이 적용된 핸들러함수를 resize이벤트에 연결 및 제거
 		window.addEventListener('resize', throttledInitPos);
 		return () => window.removeEventListener('resize', throttledInitPos);
-	}, [Index, throttledInitPos, createMap]);
+	}, [throttledInitPos, createMap]);
 
 	useEffect(() => {
 		Traffic ? ref_instMap.current.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC) : ref_instMap.current.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
@@ -86,7 +89,15 @@ export default function Map() {
 			<nav className='btnSet'>
 				<ul className='branch'>
 					{ref_info.current.map((el, idx) => (
-						<li key={idx} className={idx === Index ? 'on' : ''} onClick={() => setIndex(idx)}>
+						<li
+							key={idx}
+							className={idx === Index ? 'on' : ''}
+							onClick={() => {
+								//실제 각 지점 버튼 클릭시 Traffic, Roadview 상태값 초기화처리
+								setIndex(idx);
+								setTraffic(false);
+								setRoadview(false);
+							}}>
 							{el.title}
 						</li>
 					))}
@@ -104,10 +115,3 @@ export default function Map() {
 		</section>
 	);
 }
-
-/*
-	throttle 
-	- throttle의 개념 : 물리적으로 일정시간동안의 함수 호출을 줄여서 성능개선
-	- throttle 사용하는 주된 경우 : 단기간에 이벤트가 많이 발생하는 resize, scroll, mousemove등에 연결되는 핸들러함수를 throttle처리
-	- throttle은 이벤트의 발생자체를 줄이는 것이 아닌, 이벤트에 연결되어 있는 핸들러함수의 호출자체를 줄임
-*/
