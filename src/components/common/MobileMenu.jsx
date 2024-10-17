1a4608397bc79013f7541af0c5a01ed6cd7846b2
mania605 <kim.minsun83@gmail.com>
2024-10-17 17:15:28 +0900

    모바일 패널 브라우저 리사이즈시 자동 닫기 처리

--- src/components/common/MobileMenu.jsx
+++ src/components/common/MobileMenu.jsx
@@ -1,10 +1,22 @@
+import { useEffect } from 'react';
 import { useGlobalState } from '../../hooks/useGlobal';
+import { motion } from 'framer-motion';
+import useThrottle from '../../hooks/useThrottle';
 
 export default function MobileMenu() {
 	const { dispatch } = useGlobalState();
+	const closeMenu = () => {
+		console.log('closeMenu');
+		if (window.innerWidth >= 1000) dispatch({ type: 'CLOSE' });
+	};
+	const throttledCloseMenu = useThrottle(closeMenu);
+	useEffect(() => {
+		window.addEventListener('resize', throttledCloseMenu);
+		return () => window.removeEventListener('resize', throttledCloseMenu);
+	}, [throttledCloseMenu]);
 	return (
-		<aside className='mobileMenu' onClick={() => dispatch({ type: 'CLOSE' })}>
+		<motion.aside className='mobileMenu' onClick={() => dispatch({ type: 'CLOSE' })}>
 			MobileMenu
-		</aside>
+		</motion.aside>
 	);
 }
