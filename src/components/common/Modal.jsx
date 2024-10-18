8988755b4a69aa1c7a55f36f71ae3d8ed07ab797
mania605 <kim.minsun83@gmail.com>
2024-10-18 10:57:57 +0900

    Modal컴포넌트안쪽에 useGlobalDispatch호출시 ()누락 수정

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -2,7 +2,7 @@ import { ACTIONS, useGlobalDispatch } from '../../hooks/useGlobal';
 import { motion } from 'framer-motion';
 
 export default function Modal({ children }) {
-	const { dispatch } = useGlobalDispatch;
+	const { dispatch } = useGlobalDispatch();
 	return (
 		<motion.aside className='modal'>
 			<div className='con'>{children}</div>
